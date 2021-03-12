80cd3f8788e602202dc1fd33960fe3970a9ddc02
freearhey <free.arhey@gmail.com>
2021-03-12 18:45:32 +0300

    Rename to run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+# ./bin/epg-grabber/index.js --config=sites/tv.yandex.ru.config.js && \
+# ./bin/epg-grabber/index.js --config=sites/tvguide.co.uk.config.js && \
+./bin/epg-grabber/index.js --config=sites/programme-tv.net.config.js
