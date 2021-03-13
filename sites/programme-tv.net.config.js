10a19c45d1ed1eb24faf4f359c4753b4237598ed
freearhey <free.arhey@gmail.com>
2021-03-13 20:28:29 +0300

    Update programme-tv.net.config.js

--- sites/programme-tv.net.config.js
+++ sites/programme-tv.net.config.js
@@ -14,7 +14,7 @@ module.exports = {
   lang: 'fr',
   site: 'programme-tv.net',
   channels: 'programme-tv.net.channels.xml',
-  output: '.gh-pages/guides/programme-tv.net.xml',
+  output: '.gh-pages/guides/programme-tv.net.guide.xml',
   url: function ({ date, channel }) {
     return `https://www.programme-tv.net/programme/chaine/${date.format('YYYY-MM-DD')}/programme-${
       channel.site_id
