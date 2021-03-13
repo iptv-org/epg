d6eed75157d8f2ce88dc7be2cafecf4a65036799
freearhey <free.arhey@gmail.com>
2021-03-13 21:26:05 +0300

    Remove tvguide.co.uk

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
 npx epg-grabber --config=sites/programme-tv.net.config.js && \
-npx epg-grabber --config=sites/tvguide.co.uk.config.js && \
 npx epg-grabber --config=sites/ontvtonight.com.config.js && \
 npx epg-grabber --config=sites/tv.yandex.ru.config.js
