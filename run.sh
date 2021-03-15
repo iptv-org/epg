13bb8066393bcd3d3946b63c9bedd8689f7d2714
freearhey <free.arhey@gmail.com>
2021-03-15 16:26:12 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-npx epg-grabber --config=sites/programme-tv.net.config.js && \
-npx epg-grabber --config=sites/ontvtonight.com.config.js && \
-npx epg-grabber --config=sites/tv.yandex.ru.config.js
