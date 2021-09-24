a981e412c3f547228fe915f15f364788c0c92411
Aleksandr Statciuk <free.arhey@gmail.com>
2021-09-24 15:39:40 +0300

    Update andorradifusio.ad.config.js

--- sites/andorradifusio.ad.config.js
+++ sites/andorradifusio.ad.config.js
@@ -27,7 +27,7 @@ module.exports = {
       let start = parseStart(item, date)
       if (start.hour() > 11) PM = true
       if (start.hour() < 12 && PM) start = start.add(1, 'd')
-      const stop = parseStop(item, date)
+      const stop = start.add(1, 'h')
       if (programs.length) {
         programs[programs.length - 1].stop = start
       }
@@ -41,10 +41,6 @@ module.exports = {
 
     return programs
   }
-}
-
-function parseStop(item, date) {
-  return date.tz('Europe/Madrid').endOf('d').add(6, 'h')
 }
 
 function parseStart(item, date) {
