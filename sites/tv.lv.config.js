df30d33c68559f8bc38fa8ce9c62b462fcab2106
Aleksandr Statciuk <free.arhey@gmail.com>
2021-10-05 03:33:34 +0300

    Update tv.lv.config.js

--- sites/tv.lv.config.js
+++ sites/tv.lv.config.js
@@ -1,10 +1,7 @@
 const dayjs = require('dayjs')
 
 module.exports = {
-  lang: 'lv',
   site: 'tv.lv',
-  channels: 'tv.lv.channels.xml',
-  output: '.gh-pages/guides/tv.lv.guide.xml',
   url: function ({ date, channel }) {
     return `https://www.tv.lv/programme/listing/none/${date.format(
       'DD-MM-YYYY'
