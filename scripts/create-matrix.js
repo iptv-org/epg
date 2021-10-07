8f830182f09b35fa89932d28ba1d11d95292b738
Aleksandr Statciuk <free.arhey@gmail.com>
2021-10-07 22:59:38 +0300

    Update create-matrix.js

--- scripts/create-matrix.js
+++ scripts/create-matrix.js
@@ -1,6 +1,6 @@
 const file = require('./file.js')
 
-file.list(['ad'], ['us-local']).then(files => {
+file.list('channels/**/*.xml', ['ad'], ['us-local']).then(files => {
   const country = files.map(file => file.replace(/channels\/|\.xml/gi, ''))
   const days = country.map(() => 2)
   const matrix = { country, days }
