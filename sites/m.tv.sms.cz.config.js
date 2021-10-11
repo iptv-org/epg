f348685a81fdcdf21cc993ccee6e2e9b8787a1d5
Aleksandr Statciuk <github@oiya.xyz>
2021-10-11 21:19:15 +0300

    Update m.tv.sms.cz.config.js

--- sites/m.tv.sms.cz.config.js
+++ sites/m.tv.sms.cz.config.js
@@ -11,7 +11,6 @@ dayjs.extend(timezone)
 dayjs.extend(customParseFormat)
 
 module.exports = {
-  lang: 'cz',
   site: 'm.tv.sms.cz',
   url: function ({ date, channel }) {
     return `https://m.tv.sms.cz/index.php?stanice=${channel.site_id}&cas=0&den=${date.format(
