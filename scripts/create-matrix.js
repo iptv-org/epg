df3bb4014da7fdcc02c6ceab87933ba642701ef1
Aleksandr Statciuk <github@oiya.xyz>
2021-10-12 01:42:32 +0300

    Update create-matrix.js

--- scripts/create-matrix.js
+++ scripts/create-matrix.js
@@ -1,9 +1,26 @@
-const file = require('./file.js')
+const file = require('./file')
+const parser = require('./parser')
 
-file.list('channels/**/*.xml', ['ad'], ['us-local']).then(files => {
-  const country = files.map(file => file.replace(/channels\/|\.xml/gi, ''))
-  const days = country.map(() => 2)
-  const matrix = { country, days }
+const days = 2
+const includes = []
+
+file.list('sites/*.channels.xml', includes).then(files => {
+  const matrix = {
+    site: [],
+    country: [],
+    days: []
+  }
+
+  files.forEach(filename => {
+    const channelsFile = file.read(filename)
+    const parsed = parser.parseChannels(channelsFile)
+    parsed.groups.forEach(group => {
+      matrix.site.push(parsed.site)
+      matrix.country.push(group.country)
+      matrix.days.push(days)
+    })
+  })
+
   const output = `::set-output name=matrix::${JSON.stringify(matrix)}`
   console.log(output)
 })
