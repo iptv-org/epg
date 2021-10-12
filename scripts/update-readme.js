c24d5ab09234b52da48bba52d355153734a9a853
Aleksandr Statciuk <github@oiya.xyz>
2021-10-12 21:16:56 +0300

    Update update-readme.js

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -2,15 +2,39 @@ const markdownInclude = require('markdown-include')
 const path = require('path')
 const fs = require('fs')
 const countries = require('./countries.json')
-const file = require('./file.js')
+const parser = require('./parser')
+const file = require('./file')
 
 async function main() {
   console.log('Starting...')
   file
-    .list('channels/**/*.xml', [], ['us-local'])
+    .list('sites/*.channels.xml')
     .then(files => {
-      files = files.map(str => str.match(/channels\/(.*).xml/i)[1])
-      generateTable(files)
+      let data = []
+      files.forEach(filename => {
+        const channelsFile = file.read(filename)
+        const parsed = parser.parseChannels(channelsFile)
+        parsed.groups.forEach(group => {
+          const country = countries.find(c => c.code === group.country.toLowerCase())
+          data.push({
+            countryFlag: country.flag,
+            countryName: country.name,
+            guideUrl: `https://iptv-org.github.io/epg/guides/${country.code}/${parsed.site}.epg.xml`,
+            channelCount: group.channels.length
+          })
+        })
+      })
+
+      data = data.sort((a, b) => {
+        var nameA = a.countryName.toLowerCase()
+        var nameB = b.countryName.toLowerCase()
+        if (nameA < nameB) return -1
+        if (nameA > nameB) return 1
+        return b.channelCount - a.channelCount
+      })
+
+      const table = generateTable(data, ['Country', 'Channels', 'EPG'])
+      file.write('./.readme/_table.md', table)
       generateReadme()
     })
     .finally(() => {
@@ -18,50 +42,24 @@ async function main() {
     })
 }
 
-function generateTable(files) {
-  console.log('Generating countries table...')
-
-  const output = []
-  for (const country of countries) {
-    if (files.includes(country.code)) {
-      output.push({
-        country: `${country.flag}&nbsp;${country.name}`,
-        guide: `<code>https://iptv-org.github.io/epg/guides/${country.code}.xml</code>`
-      })
-    }
-  }
-
-  const table = generateHtmlTable(output, {
-    columns: [
-      { name: 'Country', align: 'left', nowrap: true },
-      { name: 'EPG', align: 'left', nowrap: true }
-    ]
-  })
-
-  fs.writeFileSync(path.resolve('./.readme/_table.md'), table)
-}
-
-function generateHtmlTable(data, options) {
+function generateTable(data, header) {
   let output = '<table>\n'
 
   output += '\t<thead>\n\t\t<tr>'
-  for (let column of options.columns) {
-    output += `<th align="${column.align}">${column.name}</th>`
+  for (let column of header) {
+    output += `<th align="left">${column}</th>`
   }
   output += '</tr>\n\t</thead>\n'
 
   output += '\t<tbody>\n'
   for (let item of data) {
-    output += '\t\t<tr>'
-    let i = 0
-    for (let prop in item) {
-      const column = options.columns[i]
-      let nowrap = column.nowrap
-      let align = column.align
-      output += `<td align="${align}"${nowrap ? ' nowrap' : ''}>${item[prop]}</td>`
-      i++
-    }
-    output += '</tr>\n'
+    const size = data.filter(i => i.countryName === item.countryName).length
+    let root = output.indexOf(item.countryName) === -1
+    const rowspan = root && size > 1 ? ` rowspan="${size}"` : ''
+    const cell1 = root
+      ? `<td align="left" valign="top" nowrap${rowspan}>${item.countryFlag}&nbsp;${item.countryName}</td>`
+      : ''
+    output += `\t\t<tr>${cell1}<td align="right" nowrap>${item.channelCount}</td><td align="left" nowrap><code>${item.guideUrl}</code></td></tr>\n`
   }
   output += '\t</tbody>\n'
 
