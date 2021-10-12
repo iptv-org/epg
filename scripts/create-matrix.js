b2a93876ab0dd7b5fd661245564fa4da71121c56
Aleksandr Statciuk <github@oiya.xyz>
2021-10-13 01:27:03 +0300

    Update create-matrix.js

--- scripts/create-matrix.js
+++ scripts/create-matrix.js
@@ -1,21 +1,26 @@
 const file = require('./file')
 const parser = require('./parser')
 
-file.list('sites/*.channels.xml', ['sites/andorradifusio.ad.channels.xml']).then(files => {
-  const matrix = {
-    site: [],
-    country: []
-  }
+file
+  .list('sites/*.channels.xml', [
+    'sites/andorradifusio.ad.channels.xml',
+    'sites/arianaafgtv.com.channels.xml'
+  ])
+  .then(files => {
+    const matrix = {
+      site: [],
+      country: []
+    }
 
-  files.forEach(filename => {
-    const channelsFile = file.read(filename)
-    const parsed = parser.parseChannels(channelsFile)
-    parsed.groups.forEach(group => {
-      matrix.site.push(parsed.site)
-      matrix.country.push(group.country.toLowerCase())
+    files.forEach(filename => {
+      const channelsFile = file.read(filename)
+      const parsed = parser.parseChannels(channelsFile)
+      parsed.groups.forEach(group => {
+        matrix.site.push(parsed.site)
+        matrix.country.push(group.country.toLowerCase())
+      })
     })
-  })
 
-  const output = `::set-output name=matrix::${JSON.stringify(matrix)}`
-  console.log(output)
-})
+    const output = `::set-output name=matrix::${JSON.stringify(matrix)}`
+    console.log(output)
+  })
