7fd0d3d0adcdd84b43ceab742bb21bfdc9bd5a11
Aleksandr Statciuk <github@oiya.xyz>
2021-10-27 22:24:46 +0300

    Update dstv.com.test.js

--- sites/dstv.com/dstv.com.test.js
+++ sites/dstv.com/dstv.com.test.js
@@ -1,1 +1,1 @@
-// npx epg-grabber --config=sites/dstv.com/dstv.com.config.js --channels=sites/dstv.com/dstv.com_bf.channels.xml --days=1 --output=.gh-pages/guides/bf/dstv.com.epg.xml
+// npx epg-grabber --config=sites/dstv.com/dstv.com.config.js --channels=sites/dstv.com/dstv.com_ao.channels.xml --days=1 --output=.gh-pages/guides/ao/dstv.com.epg.xml
