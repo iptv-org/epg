bb739dcdc7bf6958e4265682c552f39b530ae4cd
Aleksandr Statciuk <github@oiya.xyz>
2021-10-29 23:58:57 +0300

    Update turksatkablo.com.tr.config.js

--- sites/turksatkablo.com.tr/turksatkablo.com.tr.config.js
+++ sites/turksatkablo.com.tr/turksatkablo.com.tr.config.js
@@ -1,7 +1,9 @@
 const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
 
+dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.extend(customParseFormat)
 
