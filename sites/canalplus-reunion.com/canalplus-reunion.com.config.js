3a9c49461d958ae62da15d5150fd912b40637b64
Aleksandr Statciuk <github@oiya.xyz>
2021-11-13 15:49:17 +0300

    Create canalplus-reunion.com.config.js

---
+++ sites/canalplus-reunion.com/canalplus-reunion.com.config.js
@@ -0,0 +1,47 @@
+const dayjs = require('dayjs')
+
+module.exports = {
+  site: 'canalplus-reunion.com',
+  url: function ({ channel, date }) {
+    const diff = date.diff(dayjs().startOf('d'), 'd')
+
+    return `https://service.canal-overseas.com/ott-frontend/vector/63001/channel/${channel.site_id}/events?filter.day=${diff}`
+  },
+  logo({ channel }) {
+    return channel.logo
+  },
+  parser: function ({ content }) {
+    let programs = []
+    const items = parseItems(content)
+    items.forEach(item => {
+      if (item.title === 'Fin des programmes') return
+      programs.push({
+        title: item.title,
+        icon: item.URLImageDefault,
+        start: parseStart(item).toJSON(),
+        stop: parseStop(item).toJSON()
+      })
+    })
+
+    return programs
+  }
+}
+
+function parseStart(item) {
+  return dayjs.unix(item.startTime)
+}
+
+function parseStop(item) {
+  return dayjs.unix(item.endTime)
+}
+
+function parseItems(content) {
+  const data = JSON.parse(content)
+  if (!data || !data.timeSlices) return []
+  const items = data.timeSlices.reduce((acc, curr) => {
+    acc = acc.concat(curr.contents)
+    return acc
+  }, [])
+
+  return items
+}
