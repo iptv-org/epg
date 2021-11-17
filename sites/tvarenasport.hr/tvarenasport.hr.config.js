386c22d7732e82ad974ced9025e0f6d495bd6750
Aleksandr Statciuk <github@oiya.xyz>
2021-11-17 16:23:26 +0300

    Create tvarenasport.hr.config.js

---
+++ sites/tvarenasport.hr/tvarenasport.hr.config.js
@@ -0,0 +1,49 @@
+const axios = require('axios')
+const dayjs = require('dayjs')
+
+module.exports = {
+  site: 'tvarenasport.hr',
+  url: function ({ channel, date }) {
+    return `https://www.tvarenasport.hr/api/schedule?date=${date.format('DD-MM-YYYY')}`
+  },
+  parser: function ({ content, channel }) {
+    let programs = []
+    const items = parseItems(content, channel)
+    items.forEach(item => {
+      programs.push({
+        title: item.title.trim(),
+        category: item.sport,
+        description: item.league.trim(),
+        start: dayjs(item.start),
+        stop: dayjs(item.end)
+      })
+    })
+
+    return programs
+  },
+  async channels({ country, lang }) {
+    const data = await axios
+      .get(`https://www.tvarenasport.hr/api/schedule`)
+      .then(r => r.data)
+      .catch(console.log)
+
+    const channels = []
+    for (let id in data.channels) {
+      const item = data.channels[id]
+      channels.push({
+        lang: 'hr',
+        site_id: id,
+        name: item.name
+      })
+    }
+
+    return channels
+  }
+}
+
+function parseItems(content, channel) {
+  const data = JSON.parse(content)
+  if (!data || !Array.isArray(data.items)) return []
+
+  return data.items.filter(i => i.group === channel.site_id)
+}
