eecb4ba4486aac9fe4313e23cad00f0c33775754
Aleksandr Statciuk <github@oiya.xyz>
2021-11-18 17:59:50 +0300

    Update tv.cctv.com.config.js

--- sites/tv.cctv.com/tv.cctv.com.config.js
+++ sites/tv.cctv.com/tv.cctv.com.config.js
@@ -2,9 +2,6 @@ const dayjs = require('dayjs')
 
 module.exports = {
   site: 'tv.cctv.com',
-  request: {
-    timeout: 30000
-  },
   url({ channel, date }) {
     return `https://api.cntv.cn/epg/getEpgInfoByChannelNew?serviceId=tvcctv&c=${
       channel.site_id
