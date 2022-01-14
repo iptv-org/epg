091d919c60811bdcc3035e62a7d4a300a3d0a8aa
Aleksandr Statciuk <github@oiya.xyz>
2022-01-15 00:17:18 +0300

    Update create-database.test.js

--- tests/commands/create-database.test.js
+++ tests/commands/create-database.test.js
@@ -15,31 +15,34 @@ beforeEach(() => {
 it('can create channels database', () => {
   const output = content('tests/__data__/output/database/channels.db')
 
-  expect(output[0]).toMatchObject({
-    lang: 'en',
-    country: 'LV',
-    xmltv_id: '1Plus2.lv',
-    site_id: '1341',
-    name: '1+2',
-    site: 'example.com',
-    channelsPath: 'tests/__data__/input/sites/example.com_en-ee.channels.xml',
-    configPath: 'tests/__data__/input/sites/example.com.config.js',
-    gid: 'en-ee',
-    cluster_id: 1
-  })
-
-  expect(output[1]).toMatchObject({
-    lang: 'ru',
-    country: 'US',
-    xmltv_id: 'CNNInternationalEurope.us',
-    site_id: '140',
-    name: 'CNN International Europe',
-    site: 'example.com',
-    channelsPath: 'tests/__data__/input/sites/example.com_ca-nl.channels.xml',
-    configPath: 'tests/__data__/input/sites/example.com.config.js',
-    gid: 'ca-nl',
-    cluster_id: 1
-  })
+  expect(output).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        lang: 'en',
+        country: 'LV',
+        xmltv_id: '1Plus2.lv',
+        site_id: '1341',
+        name: '1+2',
+        site: 'example.com',
+        channelsPath: 'tests/__data__/input/sites/example.com_en-ee.channels.xml',
+        configPath: 'tests/__data__/input/sites/example.com.config.js',
+        gid: 'en-ee',
+        cluster_id: 1
+      }),
+      expect.objectContaining({
+        lang: 'ru',
+        country: 'US',
+        xmltv_id: 'CNNInternationalEurope.us',
+        site_id: '140',
+        name: 'CNN International Europe',
+        site: 'example.com',
+        channelsPath: 'tests/__data__/input/sites/example.com_ca-nl.channels.xml',
+        configPath: 'tests/__data__/input/sites/example.com.config.js',
+        gid: 'ca-nl',
+        cluster_id: 1
+      })
+    ])
+  )
 })
 
 function content(filepath) {
