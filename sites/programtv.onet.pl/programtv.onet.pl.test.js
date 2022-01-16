337d0b412debe9680efc9d0e6c7113d735abda53
Aleksandr Statciuk <github@oiya.xyz>
2022-01-16 17:48:29 +0300

    Update programtv.onet.pl.test.js

--- sites/programtv.onet.pl/programtv.onet.pl.test.js
+++ sites/programtv.onet.pl/programtv.onet.pl.test.js
@@ -1,4 +1,4 @@
-// npx epg-grabber --config=sites/programtv.onet.pl/programtv.onet.pl.config.js --channels=sites/programtv.onet.pl/programtv.onet.pl_pl.channels.xml --output=.gh-pages/guides/pl/programtv.onet.pl.epg.xml --days=2
+// npx epg-grabber --config=sites/programtv.onet.pl/programtv.onet.pl.config.js --channels=sites/programtv.onet.pl/programtv.onet.pl_pl.channels.xml --output=guide.xml --days=2
 
 const MockDate = require('mockdate')
 const { parser, url, logo } = require('./programtv.onet.pl.config.js')
@@ -16,7 +16,7 @@ const channel = {
 const content = `<!DOCTYPE html><html lang="pl"> <head></head> <body class="withFilters pageChannel"> <div id="channelPage"> <div id="channelTV" class="nextToMenu"> <section class="channelEmissions"> <header> <span class="logoTV"> <img src="//ocdn.eu/ptv2-images-transforms/1/zB4kr1sb2dvLW1pZ3JhdGVkLzEzdGgtc3RyZWV0LnBuZ5KVAmQAwsOVAgAowsM" alt="13th Street"/> </span> </header> <div class="emissions"> <ul> <li class="hh03 hh04 fltrSerie"> <div class="hours"> <span class="hour">03:20</span> </div><div class="titles"> <a href="/tv/law-and-order-odcinek-15/rlmzu?entry=21970867" >Law &amp; Order, odc. 15: Letzte Worte</a > <span class="type">Krimiserie</span> <p> Bei einer Reality-TV-Show stirbt einer der Teilnehmer. Zunächst tappen Briscoe (Jerry Orbach) und Green (Jesse L.... </p></div></li><li class="hh23 hh00 fltrSerie"> <div class="hours"> <span class="hour">23:30</span> </div><div class="titles"> <a href="/tv/navy-cis-odcinek-1/73vbw?entry=22035734" >Navy CIS, odc. 1: New Orleans</a > <span class="type">Krimiserie</span> <p> Der Abgeordnete Dan McLane, ein ehemaliger Vorgesetzter von Gibbs, wird in New Orleans ermordet. In den 90er Jahren... </p></div></li><li class="hh01 fltrSerie"> <div class="hours"> <span class="hour">01:00</span> </div><div class="titles"> <a href="/tv/navy-cis-la-odcinek-13/tuc34?entry=22035821" >Navy CIS: L.A, odc. 13: High Society</a > <span class="type">Krimiserie</span> <p> Die Zahl der Drogentoten ist gestiegen. Das Team des NCIS glaubt, dass sich Terroristen durch den zunehmenden... </p></div></li></ul> </div></section> </div></div></body></html>`
 
 it('can generate valid url', () => {
-  MockDate.set(new Date('2021-11-24'))
+  MockDate.set(dayjs.utc('2021-11-24', 'YYYY-MM-DD').startOf('d'))
   expect(url({ channel, date })).toBe(
     'https://programtv.onet.pl/program-tv/13th-street-250?dzien=0'
   )
@@ -24,7 +24,7 @@ it('can generate valid url', () => {
 })
 
 it('can generate valid url for next day', () => {
-  MockDate.set(new Date('2021-11-25'))
+  MockDate.set(dayjs.utc('2021-11-25', 'YYYY-MM-DD').startOf('d'))
   expect(url({ channel, date })).toBe(
     'https://programtv.onet.pl/program-tv/13th-street-250?dzien=1'
   )
