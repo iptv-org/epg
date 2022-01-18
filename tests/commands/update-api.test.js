a36866738111662ecaf9d97b3462d54952df8ea8
Aleksandr Statciuk <github@oiya.xyz>
2022-01-18 22:19:44 +0300

    Update update-api.test.js

--- tests/commands/update-api.test.js
+++ tests/commands/update-api.test.js
@@ -10,6 +10,10 @@ beforeEach(() => {
     'tests/__data__/input/database/channels.db',
     'tests/__data__/temp/database/channels.db'
   )
+  fs.copyFileSync(
+    'tests/__data__/input/database/programs.db',
+    'tests/__data__/temp/database/programs.db'
+  )
 
   execSync(
     'DB_DIR=tests/__data__/temp/database API_DIR=tests/__data__/output/api node scripts/commands/update-api.js',
@@ -24,6 +28,13 @@ afterEach(() => {
 it('can generate channels.json', () => {
   const output = content('tests/__data__/output/api/channels.json')
   const expected = content('tests/__data__/expected/api/channels.json')
+
+  expect(output).toBe(expected)
+})
+
+it('can generate programs.json', () => {
+  const output = content('tests/__data__/output/api/programs.json')
+  const expected = content('tests/__data__/expected/api/programs.json')
 
   expect(output).toBe(expected)
 })
