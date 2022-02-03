627792d45ccab42a266c00a006ff481dd69991a7
Aleksandr Statciuk <github@oiya.xyz>
2022-02-03 21:02:18 +0300

    Update update-api.test.js

--- tests/commands/update-api.test.js
+++ tests/commands/update-api.test.js
@@ -5,33 +5,16 @@ const { execSync } = require('child_process')
 beforeEach(() => {
   fs.rmdirSync('tests/__data__/output', { recursive: true })
   fs.mkdirSync('tests/__data__/output')
-  fs.mkdirSync('tests/__data__/temp/database', { recursive: true })
-  fs.copyFileSync('tests/__data__/input/database/queue.db', 'tests/__data__/temp/database/queue.db')
-  fs.copyFileSync(
-    'tests/__data__/input/database/programs.db',
-    'tests/__data__/temp/database/programs.db'
-  )
 
   const stdout = execSync(
-    'DB_DIR=tests/__data__/temp/database API_DIR=tests/__data__/output/api node scripts/commands/update-api.js',
+    'CHANNELS_PATH=tests/__data__/input/sites/**.channels.xml OUTPUT_DIR=tests/__data__/output/api node scripts/commands/update-api.js',
     { encoding: 'utf8' }
   )
-})
-
-afterEach(() => {
-  fs.rmdirSync('tests/__data__/temp', { recursive: true })
 })
 
 it('can generate guides.json', () => {
   const output = content('tests/__data__/output/api/guides.json')
   const expected = content('tests/__data__/expected/api/guides.json')
-
-  expect(output).toBe(expected)
-})
-
-it('can generate programs.json', () => {
-  const output = content('tests/__data__/output/api/programs.json')
-  const expected = content('tests/__data__/expected/api/programs.json')
 
   expect(output).toBe(expected)
 })
