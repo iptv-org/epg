eb182362c3cddb3923005f2c00268158987d9b67
Aleksandr Statciuk <github@oiya.xyz>
2022-02-27 09:18:51 +0300

    Update parse.js

--- scripts/commands/channels/parse.js
+++ scripts/commands/channels/parse.js
@@ -1,5 +1,5 @@
+const { db, logger } = require('../../core')
 const { Command } = require('commander')
-const { db, logger } = require('../core')
 const path = require('path')
 const _ = require('lodash')
 const fs = require('fs')
