47e272ccc00152955e363a10eddd4cf757774c5c
Aleksandr Statciuk <github@oiya.xyz>
2022-03-15 15:26:15 +0300

    Create zip.js

---
+++ scripts/core/zip.js
@@ -0,0 +1,13 @@
+const { gzip, ungzip } = require('node-gzip')
+
+const zip = {}
+
+zip.compress = async function (string) {
+	return gzip(string)
+}
+
+zip.decompress = async function (string) {
+	return ungzip(string)
+}
+
+module.exports = zip
