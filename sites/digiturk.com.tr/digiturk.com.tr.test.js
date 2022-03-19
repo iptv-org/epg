0c0a6e3480156c29a402a8d3807ab52310703001
Aleksandr Statciuk <github@oiya.xyz>
2022-03-19 20:49:32 +0300

    Fixes validation issues

--- sites/digiturk.com.tr/digiturk.com.tr.test.js
+++ sites/digiturk.com.tr/digiturk.com.tr.test.js
@@ -10,7 +10,7 @@ dayjs.extend(utc)
 const date = dayjs.utc('2021-11-10', 'YYYY-MM-DD').startOf('d')
 const channel = {
   site_id: '483',
-  xmltv_id: 'BeInTurkiye.qa'
+  xmltv_id: 'BeInTurkey.qa'
 }
 const content = `{"listings":{"483":[{"ProgramId":null,"ChannelId":483,"ProgramName":"MAÇA KIZI","OrginalName":"QUEEN OF SPADES (2021)","BroadcastStart":"2021-11-10T23:25:00","BroadcastEnd":"2021-11-11T00:53:00","BroadcastDuration":5245,"PartNo":null,"HasSubtitle":false,"ProgramLanguage":null,"ScreenRatio":"4:3","IsLive":false,"Synopsis":"[QUEEN OF SPADES] DÖRT GENÇ, EFSANEYE GÖRE MAÇA KIZI OLARAK BİLİNEN BİR RUHU ÇAĞIRMAK İÇİN RİTÜEL GERÇEKLEŞTİRİRLER.BU RİTÜELİN SONUNDA KORKU DOLU ANLAR YAŞANACAKTIR.","CreatedBy":"EPG Import Service","CreatedDate":"2021-11-10T10:47:00","UpdatedBy":"EPG Import Service","LastModifyDate":"2021-11-10T10:47:00","LastIP":null,"Genre":"E8","Rating":"D6","Year":"2021","Actors":"AVA PRESTON , KAELEN OHM , JAMIE BLOCH ,","SeriesId":null,"SeasonId":null,"LongDescription":"PATRICK WHITE'IN İLK YÖNETMENLİK DENEYİMİ OLAN BU KORKU FİLMİNİN BAŞROLLERİNDE AVA PRESTON VE KAELEN OHM YER ALIYOR.","ServiceRef":"7808","ContentRef":"PV0000340386","EventId":"8432","ScreenSize":null,"AudioType":null,"BroadcastTimeStamp":1636586700,"Directors":"PATRICK WHITE ,","ProductionCountries":"CAN","MasterProductionID":"PT0000324047","EPGBroadcastID":"LYS188925910","Id":1404880537}]}}`
 
