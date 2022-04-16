6604d1f4aedc70f42ead9d096e1a34f9081c4b48
Aleksandr Statciuk <github@oiya.xyz>
2022-04-16 15:03:18 +0300

    Fixes validation errors

--- sites/magticom.ge/magticom.ge.test.js
+++ sites/magticom.ge/magticom.ge.test.js
@@ -11,7 +11,7 @@ dayjs.extend(utc)
 const date = dayjs.utc('2021-11-22', 'YYYY-MM-DD').startOf('d')
 const channel = {
   site_id: '260',
-  xmltv_id: 'BollywoodTVRossiya.ru'
+  xmltv_id: 'BollywoodHD.ru'
 }
 
 it('can generate valid url', () => {
