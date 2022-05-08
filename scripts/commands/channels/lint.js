d5ca9983fb520e2ec73ee1d57b742306812b199b
Aleksandr Statciuk <github@oiya.xyz>
2022-05-08 13:28:17 +0300

    Replace libxmljs with libxmljs2

--- scripts/commands/channels/lint.js
+++ scripts/commands/channels/lint.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk')
-const libxml = require('libxmljs')
+const libxml = require('libxmljs2')
 const { program } = require('commander')
 const { logger, file } = require('../../core')
 
