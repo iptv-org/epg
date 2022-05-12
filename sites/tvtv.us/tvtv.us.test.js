3173b6b9090b20304bdaca74166ca78cacfed4cc
Arkina <60531069+Arkina1234@users.noreply.github.com>
2022-05-12 07:59:59 -0400

    Update tvtv.us.test.js

--- sites/tvtv.us/tvtv.us.test.js
+++ sites/tvtv.us/tvtv.us.test.js
@@ -16,12 +16,12 @@ const channel = {
 
 it('can generate valid url', () => {
   expect(url({ channel, date })).toBe(
-    'https://www.tvtv.us/gn/d/v1.1/stations/62670/airings?startDateTime=2022-01-17T00:00:00Z&endDateTime=2022-01-18T00:00:00Z'
+    'https://www.tvtv.us/gn/d/v1.1/stations/62670/airings?startDateTime=2022-05-12T00:00:00Z&endDateTime=2022-05-13T00:00:00Z'
   )
 })
 
 it('can parse response', () => {
-  const content = `[{"startTime":"2022-01-20T00:00Z","endTime":"2022-01-20T00:30Z","duration":30,"qualifiers":["CC","DVS"],"program":{"tmsId":"EP031727440006","rootId":"16777148","seriesId":"16640522","subType":"Series","title":"Reflect and Renew With Kevin Naidoo","episodeTitle":"Empowerment","releaseYear":2019,"releaseDate":"2019-04-20","origAirDate":"2019-04-20","titleLang":"en","descriptionLang":"en","entityType":"Episode","genres":["Health"],"longDescription":"Kevin demonstrates a meditation and yoga practice to reclaim his courage and confidence.","shortDescription":"Kevin demonstrates a meditation and yoga practice to reclaim his courage and confidence.","episodeNum":6,"seasonNum":1,"ratings":[{"body":"Régie du cinéma","code":"G"},{"body":"USA Parental Rating","code":"TVG"},{"body":"Canadian Parental Rating","code":"G"}],"preferredImage":{"width":"240","height":"360","uri":"assets/p16640522_b_v9_aa.jpg?w=240&h=360","category":"Banner-L2","text":"yes","primary":"true","tier":"Series"}},"station":{"stationId":"62670","callSign":"AMITV","videoQuality":{"signalType":"Digital","videoType":"SDTV"},"preferredImage":{"width":"360","height":"270","uri":"assets/s62670_ll_h15_ab.png?w=360&h=270","category":"Logo","primary":"true"}}},{"startTime":"2022-01-20T00:30Z","endTime":"2022-01-20T01:00Z","duration":30,"qualifiers":["CC","DVS"],"program":{"tmsId":"EP018574900040","rootId":"12448902","seriesId":"10464580","subType":"Series","title":"Four Senses","episodeTitle":"Sizzled & Seared","releaseYear":2016,"releaseDate":"2016-01-14","origAirDate":"2016-01-14","titleLang":"en","descriptionLang":"en","entityType":"Episode","genres":["House/garden"],"longDescription":"Everything is sizzled and seared as chef Corbin Tomaszeski joins Christine and Carl in the kitchen.","shortDescription":"Everything is sizzled and seared as chef Corbin Tomaszeski joins Christine and Carl in the kitchen.","topCast":["Carl Heinrich","Christine Ha"],"ratings":[{"body":"Canadian Parental Rating","code":"G"},{"body":"USA Parental Rating","code":"TVG"},{"body":"Régie du cinéma","code":"G"}],"preferredImage":{"width":"240","height":"360","uri":"assets/p10464580_b_v7_aa.jpg?w=240&h=360","category":"Banner-L1","text":"yes","primary":"true","tier":"Series"}},"station":{"stationId":"62670","callSign":"AMITV","videoQuality":{"signalType":"Digital","videoType":"SDTV"},"preferredImage":{"width":"360","height":"270","uri":"assets/s62670_ll_h15_ab.png?w=360&h=270","category":"Logo","primary":"true"}}}]`
+  const content = `[{"startTime":"2022-05-12T03:00Z","endTime":"2022-05-12T03:30Z","duration":30,"qualifiers":["CC","DVS"],"program":{"tmsId":"EP031727440001","rootId":"16640530","seriesId":"16640522","subType":"Series","title":"Reflect and Renew With Kevin Naidoo","episodeTitle":"Self-Love","releaseYear":2019,"releaseDate":"2019-03-16","origAirDate":"2019-03-16","titleLang":"en","descriptionLang":"en","entityType":"Episode","genres":["Health"],"longDescription":"Kevin demonstrates judgement-free, self-love through discussion, meditation and yoga.","shortDescription":"Kevin demonstrates judgement-free, self-love through discussion, meditation and yoga.","episodeNum":1,"seasonNum":1,"topCast":["Kevin Naidoo"],"ratings":[{"body":"Canadian Parental Rating","code":"G"},{"body":"USA Parental Rating","code":"TVG"},{"body":"Régie du cinéma","code":"G"}],"preferredImage":{"width":"240","height":"360","uri":"assets/p16640522_b_v9_aa.jpg?w=240\u0026h=360","category":"Banner-L2","text":"yes","primary":"true","tier":"Series"}},"station":{"stationId":"62670","callSign":"AMITV","videoQuality":{"signalType":"Digital","videoType":"SDTV"},"preferredImage":{"width":"360","height":"270","uri":"assets/s62670_ll_h15_ab.png?w=360\u0026h=270","category":"Logo","primary":"true"}}},{"startTime":"2022-05-12T03:30Z","endTime":"2022-05-12T04:00Z","duration":30,"qualifiers":["CC","DVS"],"program":{"tmsId":"EP018574900052","rootId":"12724343","seriesId":"10464580","subType":"Series","title":"Four Senses","episodeTitle":"Cheesy","releaseYear":2016,"releaseDate":"2016-04-07","origAirDate":"2016-03-18","titleLang":"en","descriptionLang":"en","entityType":"Episode","genres":["House/garden"],"longDescription":"Christine and Carl get cheesy with Frankie 'Flowers' Ferragine.","shortDescription":"Christine and Carl get cheesy with Frankie 'Flowers' Ferragine.","episodeNum":13,"seasonNum":3,"topCast":["Carl Heinrich","Christine Ha"],"ratings":[{"body":"Canadian Parental Rating","code":"G"},{"body":"USA Parental Rating","code":"TVG"},{"body":"Régie du cinéma","code":"G"}],"preferredImage":{"width":"240","height":"360","uri":"assets/p10464580_b_v7_aa.jpg?w=240\u0026h=360","category":"Banner-L1","text":"yes","primary":"true","tier":"Series"}},"station":{"stationId":"62670","callSign":"AMITV","videoQuality":{"signalType":"Digital","videoType":"SDTV"},"preferredImage":{"width":"360","height":"270","uri":"assets/s62670_ll_h15_ab.png?w=360\u0026h=270","category":"Logo","primary":"true"}}}]`
 
   const result = parser({ content }).map(p => {
     p.start = p.start.toJSON()
@@ -31,15 +31,21 @@ it('can parse response', () => {
 
   expect(result).toMatchObject([
     {
-      start: '2022-01-20T00:00:00.000Z',
-      stop: '2022-01-20T00:30:00.000Z',
+      start: '2022-05-12T03:00Z',
+      stop: '2022-05-12T03:30Z',
+      title: 'Reflect and Renew With Kevin Naidoo',
+      sub_title: 'Self-Love'
+      description: `Kevin demonstrates judgement-free, self-love through discussion, meditation and yoga.`,
+      date: 2019
+      category: ['Health'],
+      actor: ['Kevin Naidoo'],
       season: 1,
-      episode: 6,
+      episode: 1,
       icon: 'http://tvtv.tmsimg.com/assets/p16640522_b_v9_aa.jpg?w=240&h=360'
     },
     {
-      start: '2022-01-20T00:30:00.000Z',
-      stop: '2022-01-20T01:00:00.000Z',
+      start: '2022-05-12T03:30Z',
+      stop: '2022-05-12T04:00Z',
       title: 'Four Senses',
       description: `Everything is sizzled and seared as chef Corbin Tomaszeski joins Christine and Carl in the kitchen.`,
       category: ['House/garden'],
