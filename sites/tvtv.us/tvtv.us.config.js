27f2476b6a287c770a6e625eaea615d2c79d4e1f
Arkina <60531069+Arkina1234@users.noreply.github.com>
2022-07-07 04:07:02 -0400

    Update tvtv.us.config.js

--- sites/tvtv.us/tvtv.us.config.js
+++ sites/tvtv.us/tvtv.us.config.js
@@ -20,6 +20,7 @@ module.exports = {
         title: parseTitle(item),
         sub_title: parseSubtitle(item),
         description: parseDescription(item),
+        ratings: parseRating(item),
         category: parseCategory(item),
         season: parseSeason(item),
         episode: parseEpisode(item),
@@ -120,6 +121,14 @@ function parseSeason(item) {
 
 function parseEpisode(item) {
   return item.program.episodeNum || null
+}
+
+function parseRating(item) {
+           return {
+		system: item.program.ratings.body,
+		value: item.program.ratings.code
+	}
+}
 }
 
 function parseIcon(item) {
