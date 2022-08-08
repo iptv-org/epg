d77656a4bc26b957269493dfd1234246ffadd040
Hans5958 <hans5958@outlook.com>
2022-08-08 14:13:07 +0000

    Add UseeTV EPG

---
+++ sites/useetv.com/useetv.com.config.js
@@ -0,0 +1,71 @@
+const cheerio = require('cheerio')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
+
+module.exports = {
+  site: 'vidio.com',
+  url({ channel }) {
+    return `https://www.useetv.com/tvod/${channel.site_id}`
+  },
+  parser({ content, date }) {
+    const programs = []
+    const items = parseItems(content, date)
+    items.forEach(item => {
+      const prev = programs[programs.length - 1]
+      const $item = cheerio.load(item)
+      let start = parseStart($item, date)
+      if (prev && start.isBefore(prev.start)) {
+        start = start.add(1, 'd')
+        date = date.add(1, 'd')
+      }
+      let stop = parseStop($item, date)
+      if (stop.isBefore(start)) {
+        stop = stop.add(1, 'd')
+        date = date.add(1, 'd')
+      }
+      programs.push({
+        title: parseTitle($item),
+        start,
+        stop
+      })
+    })
+
+    return programs
+  }
+}
+
+function parseStart($item, date) {
+  const timeString = $item('p').text()
+  const [_, start] = timeString.match(/(\d{2}:\d{2}) -/) || [null, null]
+  const dateString = `${date.format('YYYY-MM-DD')} ${start}`
+
+  return dayjs.tz(dateString, 'YYYY-MM-DD HH:mm', 'Asia/Jakarta')
+}
+
+function parseStop($item, date) {
+  const timeString = $item('p').text()
+  const [_, stop] = timeString.match(/- (\d{2}:\d{2})/) || [null, null]
+  const dateString = `${date.format('YYYY-MM-DD')} ${stop}`
+
+  return dayjs.tz(dateString, 'YYYY-MM-DD HH:mm', 'Asia/Jakarta')
+}
+
+function parseTitle($item) {
+  return $item('b').text()
+}
+
+function parseItems(content, date) {
+  const $ = cheerio.load(content)
+
+  return $(
+    `#pills-${date.format(
+      'YYYY-MM-DD'
+    )} .schedule-item`
+  ).toArray()
+}
---
+++ sites/useetv.com/useetv.com.test.js
@@ -0,0 +1,55 @@
+const { parser, url, request } = require('./useetv.com.config.js')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+dayjs.extend(utc)
+
+const date = dayjs.utc('2022-08-08', 'YYYY-MM-DD').startOf('d')
+const channel = {
+  site_id: 'metrotv',
+  xmltv_id: 'MetroTV.id'
+}
+const content = `<!DOCTYPE html><html><head></head><body><section class="live-tv-channels" id="top"><div><div class="schedule-list"><div id="pills-2022-08-08"><div class="row"><div><a class="schedule-item"><span class="replay"></span><p>07:00 - 07:05</p><b>Headline News</b></a></div><div><a class="schedule-item"><span class="replay"></span><p>07:05 - 07:30</p><b>Editorial Media Indonesia</b></a></div><div><a class="schedule-item"><span class="replay"></span><p>07:30 - 07:45</p><b>Editorial Media Indonesia</b></a></div><div><a class="schedule-item"><span class="replay"></span><p>07:45 - 08:00</p><b>Editorial Media Indonesia</b></a></div></div></div></div></div></section></body>`
+
+it('can generate valid url', () => {
+  expect(url({ channel })).toBe('https://www.useetv.com/tvod/metrotv')
+})
+
+it('can parse response', () => {
+  const result = parser({ content, channel, date }).map(p => {
+    p.start = p.start.toJSON()
+    p.stop = p.stop.toJSON()
+    return p
+  })
+
+  expect(result).toMatchObject([
+    {
+      title: 'Headline News',
+      start: '2022-08-08T00:00:00.000Z',
+      stop: '2022-08-08T00:05:00.000Z'
+    },
+    {
+      title: 'Editorial Media Indonesia',
+      start: '2022-08-08T00:05:00.000Z',
+      stop: '2022-08-08T00:30:00.000Z'
+    },
+    {
+      title: 'Editorial Media Indonesia',
+      start: '2022-08-08T00:30:00.000Z',
+      stop: '2022-08-08T00:45:00.000Z'
+    },
+    {
+      title: 'Editorial Media Indonesia',
+      start: '2022-08-08T00:45:00.000Z',
+      stop: '2022-08-08T01:00:00.000Z'
+    }
+  ])
+})
+
+it('can handle empty guide', () => {
+  const result = parser({
+    date,
+    channel,
+    content: `<!DOCTYPE html><html><head></head><body></body></html>`
+  })
+  expect(result).toMatchObject([])
+})
