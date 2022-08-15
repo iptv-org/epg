105c3a862fdfadd76c27a824d43e0be9eb930cd8
Arkina <60531069+Arkina1234@users.noreply.github.com>
2022-08-15 07:03:23 -0400

    Update tvtv.us.test.js

--- sites/tvtv.us/tvtv.us.test.js
+++ sites/tvtv.us/tvtv.us.test.js
@@ -40,7 +40,7 @@ it('can parse response', () => {
       category: ['Health'],
       season: 1,
       episode: 6,
-      icon: 'https://tvtv.us/gn/i/assets/p16640522_b_v9_aa.jpg?w=240&h=360'
+      icon: 'https://tvtv.us/gn/i/assets/p16640522_b_h9_aa.jpg?w=3840&h=2880'
     },
     {
       start: '2022-01-20T00:30:00.000Z',
@@ -49,7 +49,7 @@ it('can parse response', () => {
       sub_title: 'Sizzled & Seared',
       description: `Everything is sizzled and seared as chef Corbin Tomaszeski joins Christine and Carl in the kitchen.`,
       category: ['House/garden'],
-      icon: 'https://tvtv.us/gn/i/assets/p10464580_b_v7_aa.jpg?w=240&h=360'
+      icon: 'https://tvtv.us/gn/i/assets/p10464580_b_h9_aa.jpg?w=3840&h=2880'
     }
   ])
 })
