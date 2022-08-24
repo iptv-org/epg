7b5545364d2452f5e645f6dde05e9e2ba36a8a49
Aleksandr Statciuk <github@oiya.xyz>
2022-08-24 23:02:24 +0300

    Update digiturk.com.tr.test.js

--- sites/digiturk.com.tr/digiturk.com.tr.test.js
+++ sites/digiturk.com.tr/digiturk.com.tr.test.js
@@ -1,4 +1,4 @@
-// npx epg-grabber --config=sites/digiturk.com.tr/digiturk.com.tr.config.js --channels=sites/digiturk.com.tr/digiturk.com.tr_tr.channels.xml --output=.gh-pages/guides/tr/digiturk.com.tr.epg.xml --days=2
+// npx epg-grabber --config=sites/digiturk.com.tr/digiturk.com.tr.config.js --channels=sites/digiturk.com.tr/digiturk.com.tr_tr.channels.xml --output=guide.xml --days=2
 
 const { parser, url } = require('./digiturk.com.tr.config.js')
 const dayjs = require('dayjs')
