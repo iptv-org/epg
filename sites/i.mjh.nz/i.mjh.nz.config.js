34a25853241d5bc4068280508e91d8251d0dfb47
Aleksandr Statciuk <github@oiya.xyz>
2022-11-08 01:22:38 +0300

    Update i.mjh.nz.config.js

--- sites/i.mjh.nz/i.mjh.nz.config.js
+++ sites/i.mjh.nz/i.mjh.nz.config.js
@@ -13,7 +13,7 @@ module.exports = {
     cache: {
       ttl: 6 * 60 * 60 * 1000 // 6h
     },
-    maxContentLength: 15 * 1024 * 1024 // 15Mb
+    maxContentLength: 20 * 1024 * 1024 // 20Mb
   },
   url: function ({ channel }) {
     const [source] = channel.site_id.split('#')
