696b96f73b81d66e0fa39e613e69dfcfeb68a6e1
Aleksandr Statciuk <github@oiya.xyz>
2022-12-29 23:15:57 +0300

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  singleAttributePerLine: false,
+  arrowParens: 'avoid'
+}
