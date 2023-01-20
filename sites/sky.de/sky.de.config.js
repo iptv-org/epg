1bce175a560f96cfc668c268ab308513511d6e97
RevGear <95308545+RevGear@users.noreply.github.com>
2023-01-20 18:01:01 +0000

    Update sky.de guide

--- sites/sky.de/sky.de.config.js
+++ sites/sky.de/sky.de.config.js
@@ -3,10 +3,14 @@ const dayjs = require('dayjs')
 module.exports = {
   site: 'sky.de',
   days: 2,
-  skip: true, // server returns error 403 (https://github.com/iptv-org/epg/runs/5435899744?check_suite_focus=true)
   url: `https://www.sky.de/sgtvg/service/getBroadcastsForGrid`,
   request: {
     method: 'POST',
+    headers: {
+        'accept-language': 'en-GB',
+        'accept-encoding': 'gzip, deflate, br',
+        accept: 'application/json'
+      },
     data: function ({ channel, date }) {
       return {
         cil: [channel.site_id],
