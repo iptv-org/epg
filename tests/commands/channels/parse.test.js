f2057dd0157a81c001063a669b577c2f0c74f8ce
freearhey <7253922+freearhey@users.noreply.github.com>
2023-06-30 14:41:19 +0300

    Update parse.test.js

--- tests/commands/channels/parse.test.js
+++ tests/commands/channels/parse.test.js
@@ -4,16 +4,31 @@ const path = require('path')
 
 beforeEach(() => {
   fs.emptyDirSync('tests/__data__/output')
-
-  const stdout = execSync(
-    'npm run channels:parse -- --config=tests/__data__/input/sites/parse-channels.config.js --output=tests/__data__/output/channels.xml',
-    { encoding: 'utf8' }
+  fs.copySync(
+    'tests/__data__/input/sites/parse-channels.channels.xml',
+    'tests/__data__/output/channels.xml'
   )
 })
 
 it('can parse channels', () => {
+  const stdout = execSync(
+    'npm run channels:parse -- --config=tests/__data__/input/sites/parse-channels.config.js --output=tests/__data__/output/channels.xml',
+    { encoding: 'utf8' }
+  )
+
   expect(content('tests/__data__/output/channels.xml')).toEqual(
     content('tests/__data__/expected/sites/parse-channels.channels.xml')
+  )
+})
+
+it('can parse channels with clean flag', () => {
+  const stdout = execSync(
+    'npm run channels:parse -- --config=tests/__data__/input/sites/parse-channels.config.js --output=tests/__data__/output/channels.xml --clean',
+    { encoding: 'utf8' }
+  )
+
+  expect(content('tests/__data__/output/channels.xml')).toEqual(
+    content('tests/__data__/expected/sites/parse-channels-clean.channels.xml')
   )
 })
 
