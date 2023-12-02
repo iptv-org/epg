94ca72f1989928295ac08a0d9124d6711817fdd4
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-02 18:49:40 +0300

    Update bein.com

--- sites/bein.com/bein.com.test.js
+++ sites/bein.com/bein.com.test.js
@@ -1,7 +1,3 @@
-// npm run channels:parse -- --config=./sites/bein.com/bein.com.config.js --output=./sites/bein.com/bein.com_ar.channels.xml --set=lang:ar
-// npm run channels:parse -- --config=./sites/bein.com/bein.com.config.js --output=./sites/bein.com/bein.com_en.channels.xml --set=lang:en
-// npm run grab -- --site=bein.com
-
 const fs = require('fs')
 const path = require('path')
 const { parser, url } = require('./bein.com.config.js')
