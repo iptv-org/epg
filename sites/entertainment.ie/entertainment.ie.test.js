c99424315a5612ae6819a45275aff37a3110f3a5
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-03 00:25:18 +0300

    Update entertainment.ie

--- sites/entertainment.ie/entertainment.ie.test.js
+++ sites/entertainment.ie/entertainment.ie.test.js
@@ -1,6 +1,3 @@
-// npm run channels:parse -- --config=./sites/entertainment.ie/entertainment.ie.config.js --output=./sites/entertainment.ie/entertainment.ie.channels.xml
-// npm run grab -- --site=entertainment.ie
-
 const fs = require('fs')
 const path = require('path')
 const { parser, url } = require('./entertainment.ie.config.js')
