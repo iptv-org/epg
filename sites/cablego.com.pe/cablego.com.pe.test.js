c7d421b412d8a74b57f96661df4ff8e29bd9da81
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-02 22:44:49 +0300

    Update cablego.com.pe

--- sites/cablego.com.pe/cablego.com.pe.test.js
+++ sites/cablego.com.pe/cablego.com.pe.test.js
@@ -1,6 +1,3 @@
-// npm run channels:parse -- --config=./sites/cablego.com.pe/cablego.com.pe.config.js --output=./sites/cablego.com.pe/cablego.com.pe.channels.xml
-// npm run grab -- --site=cablego.com.pe
-
 const { parser, url, request } = require('./cablego.com.pe.config.js')
 const fs = require('fs')
 const path = require('path')
