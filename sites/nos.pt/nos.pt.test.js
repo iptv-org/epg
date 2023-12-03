49c204acf210194508d5d91acdb99a884eeecb2c
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-03 18:38:55 +0300

    Update nos.pt

--- sites/nos.pt/nos.pt.test.js
+++ sites/nos.pt/nos.pt.test.js
@@ -1,6 +1,3 @@
-// npm run channels:parse -- --config=./sites/nos.pt/nos.pt.config.js --output=./sites/nos.pt/nos.pt.channels.xml
-// npm run grab -- --site=nos.pt
-
 const { parser, url } = require('./nos.pt.config.js')
 const fs = require('fs')
 const path = require('path')
