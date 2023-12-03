8ff256bcdf692ffc0be0fa4833efdaa33b86313f
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-03 17:26:13 +0300

    Update meo.pt

--- sites/meo.pt/meo.pt.test.js
+++ sites/meo.pt/meo.pt.test.js
@@ -1,6 +1,3 @@
-// npm run channels:parse -- --config=./sites/meo.pt/meo.pt.config.js --output=./sites/meo.pt/meo.pt.channels.xml
-// npm run grab -- --site=meo.pt
-
 const { parser, url, request } = require('./meo.pt.config.js')
 const fs = require('fs')
 const path = require('path')
