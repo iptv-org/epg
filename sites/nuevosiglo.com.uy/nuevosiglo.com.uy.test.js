9c09125e7f61a6ddf626487f575af9bde0cb3909
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-03 18:49:40 +0300

    Update nuevosiglo.com.uy

--- sites/nuevosiglo.com.uy/nuevosiglo.com.uy.test.js
+++ sites/nuevosiglo.com.uy/nuevosiglo.com.uy.test.js
@@ -1,6 +1,3 @@
-// [Geo-blocked] npm run channels:parse -- --config=./sites/nuevosiglo.com.uy/nuevosiglo.com.uy.config.js --output=./sites/nuevosiglo.com.uy/nuevosiglo.com.uy.channels.xml
-// [Geo-blocked] npm run grab -- --site=nuevosiglo.com.uy
-
 const { parser, url } = require('./nuevosiglo.com.uy.config.js')
 const fs = require('fs')
 const path = require('path')
