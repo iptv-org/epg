769379a707543506e4565f7ca966c61048c9006c
Toha <tohenk@yahoo.com>
2024-12-10 22:59:14 +0700

    Update startimestv.com guide.
    Test:
    
    ```sh
    npm test -- startimestv.com
    
    > test
    > run-script-os startimestv.com
    
    > test:win32
    > SET "TZ=Pacific/Nauru" && npx jest --runInBand startimestv.com
    
     PASS  sites/startimestv.com/startimestv.com.test.js
      √ can generate valid url (3 ms)
      √ can parse response (114 ms)
      √ can handle empty guide (2 ms)
    
    Test Suites: 1 passed, 1 total
    Tests:       3 passed, 3 total
    Snapshots:   0 total
    Time:        4.618 s, estimated 5 s
    Ran all test suites matching /startimestv.com/i.
    ```
    
    Grab:
    
    ```sh
    npm run grab -- --site=startimestv.com
    
    > grab
    > npx tsx scripts/commands/epg/grab.ts --site=startimestv.com
    
    starting...
    config:
      output: guide.xml
      maxConnections: 1
      gzip: false
      site: startimestv.com
    loading channels...
      found 77 channel(s)
    run #1:
      [1/154] startimestv.com (en) - 648 - Dec 10, 2024 (17 programs)
      [2/154] startimestv.com (en) - 648 - Dec 11, 2024 (18 programs)
      ...
      [153/154] startimestv.com (pt) - DisneyChannel.pt - Dec 11, 2024 (61 programs)
      [154/154] startimestv.com (en) - ZeeOneAfrica.za - Dec 10, 2024 (22 programs)
      saving to "guide.xml"...
      done in 00h 01m 02s
    ```
    
    Signed-off-by: Toha <tohenk@yahoo.com>

--- sites/startimestv.com/startimestv.com.config.js
+++ sites/startimestv.com/startimestv.com.config.js
@@ -3,108 +3,173 @@ const cheerio = require('cheerio')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
+const debug = require('debug')('site:startimestv.com')
 
 dayjs.extend(utc)
 dayjs.extend(customParseFormat)
 
+const nworker = 5
+
 module.exports = {
   site: 'startimestv.com',
   days: 2,
-  url: function ({ channel, date }) {
+  url({ channel, date }) {
     return `https://www.startimestv.com/channeldetail/${channel.site_id}/${date.format(
       'YYYY-MM-DD'
     )}.html`
   },
-  parser: function ({ content, date }) {
-    let programs = []
-    const items = parseItems(content)
-    items.forEach(item => {
-      const $item = cheerio.load(item)
-      programs.push({
-        title: parseTitle($item),
-        season: parseSeason($item),
-        episode: parseEpisode($item),
-        description: parseDescription($item),
-        start: parseStart($item, date),
-        stop: parseStop($item, date)
-      })
-    })
+  parser({ content, date }) {
+    const programs = []
+    if (content) {
+      const $ = cheerio.load(content)
+      $('.box .mask').toArray()
+        .forEach(el => {
+          let title = parseText($(el).find('h4'))
+          const [s, e] = title.substr(0, title.indexOf(' ')).split('-') || [null, null]
+          const start = dayjs.utc(`${date.format('YYYY-MM-DD')} ${s}`, 'YYYY-MM-DD HH:nn')
+          const stop = dayjs.utc(`${date.format('YYYY-MM-DD')} ${e}`, 'YYYY-MM-DD HH:nn')
+          title = title.substr(title.indexOf(' ') + 1)
+          const [, season, episode] = title.match(/ S(\d+) E(\d+)/) || [null, null, null]
+          const description = parseText($(el).find('p'))
+          programs.push({
+            title,
+            description: description !== 'NA' ? description : null,
+            season: season ? parseInt(season) : season,
+            episode: episode ? parseInt(episode) : episode,
+            start,
+            stop
+          })
+        })
+    }
 
     return programs
   },
   async channels() {
-    const _ = require('lodash')
+    const channels = {}
+    const queues = [{ t: 'a', u: 'https://www.startimestv.com/tv_guide.html' }]
+    await doFetch(queues, (queue, res) => {
+      // process area-id
+      if (queue.t === 'a') {
+        const $ = cheerio.load(res)
+        $('dd.update-areaID').toArray()
+          .forEach(el => {
+            const dd = $(el)
+            const areaId = dd.attr('area-id')
+            queues.push({
+              t: 's',
+              u: 'https://www.startimestv.com/tv_guide.html',
+              params: {
+                headers: {
+                  cookie: `default_areaID=${areaId}`
+                }
+              }
+            })
+          })
+      }
+      // process channel
+      if (queue.t === 's') {
+        if (res) {
+          const $ = cheerio.load(res)
+          $(`.channl .c`).toArray()
+            .forEach(el => {
+              // only process channel with schedule only
+              const clazz = $(el).attr('class')
+              const [idx] = clazz.match(/\d+/) || [null]
+              if (idx && $(`.item.item-${idx} .mask`).length) {
+                const ch = $(el).find('.pic a[title]')
+                const [site_id] = ch.attr('href').match(/\d+/) || [null]
+                if (channels[site_id] === undefined) {
+                  channels[site_id] = {
+                    lang: 'en',
+                    name: ch.attr('title'),
+                    site_id
+                  }
+                }
+              }
+            })
+        }
+      }
+    })
 
-    const areas = [6, 2, 3, 4, 5, 32, 14, 22, 12, 9, 16, 11, 13, 15, 20, 19]
-
-    const channels = []
-    for (let area of areas) {
-      const data = await axios
-        .get('https://www.startimestv.com/tv_guide.html', {
-          headers: {
-            Cookie: `default_areaID=${area}`
-          }
-        })
-        .then(r => r.data)
-        .catch(console.log)
-
-      const $ = cheerio.load(data)
-      const script = $('body > script:nth-child(10)').html()
-      let [, json] = script.match(/var obj = eval\( '(.*)' \);/) || [null, null]
-      json = json.replace(/\\'/g, '')
-      const items = JSON.parse(json)
-
-      items.forEach(item => {
-        channels.push({
-          lang: 'en',
-          name: item.name,
-          site_id: item.id
-        })
-      })
-    }
-
-    return _.uniqBy(channels, 'site_id')
+    return Object.values(channels)
   }
 }
 
-function parseStart($item, date) {
-  const time = $item('.in > .t').text()
-  const [, HH, mm] = time.match(/^(\d{2}):(\d{2})/) || [null, null, null]
+function parseText($item) {
+  let text = $item.text()
+    .replace(/\t/g, '')
+    .replace(/\n/g, ' ')
+    .trim()
+  while (true) {
+    if (text.match(/  /)) {
+      text = text.replace(/  /g, ' ')
+      continue
+    }
+    break
+  }
 
-  return HH && mm ? dayjs.utc(`${date.format('YYYY-MM-DD')} ${HH}:${mm}`, 'YYYY-MM-DD HH:mm') : null
+  return text
 }
 
-function parseStop($item, date) {
-  const time = $item('.in > .t').text()
-  const [, HH, mm] = time.match(/(\d{2}):(\d{2})$/) || [null, null, null]
+async function doFetch(queues, cb) {
+  const axios = require('axios')
 
-  return HH && mm ? dayjs.utc(`${date.format('YYYY-MM-DD')} ${HH}:${mm}`, 'YYYY-MM-DD HH:mm') : null
-}
-
-function parseSeason($item) {
-  const title = parseTitle($item)
-  const [, season] = title.match(/ S(\d+)/) || [null, null]
-
-  return season ? parseInt(season) : null
-}
-
-function parseEpisode($item) {
-  const title = parseTitle($item)
-  const [, episode] = title.match(/ E(\d+)/) || [null, null]
-
-  return episode ? parseInt(episode) : null
-}
-
-function parseTitle($item) {
-  return $item('.in > h3').text()
-}
-
-function parseDescription($item) {
-  return $item('.in > p').text()
-}
-
-function parseItems(content) {
-  const $ = cheerio.load(content)
-
-  return $('div.tv_gui > div.list > div > div').toArray()
+  let n = Math.min(nworker, queues.length)
+  const workers = []
+  const adjustWorker = () => {
+    if (queues.length > workers.length && workers.length < nworker) {
+      let nw = Math.min(nworker, queues.length)
+      if (n < nw) {
+        n = nw
+        createWorker()
+      }
+    }
+  }
+  const createWorker = () => {
+    while (workers.length < n) {
+      startWorker()
+    }
+  }
+  const startWorker = () => {
+    const worker = () => {
+      if (queues.length) {
+        const queue = queues.shift()
+        const done = res => {
+          if (res) {
+            cb(queue, res)
+            adjustWorker()
+          }
+          worker()
+        }
+        const url = typeof queue === 'string' ? queue : queue.u
+        const params = typeof queue === 'object' && queue.params ? queue.params : {}
+        const method = typeof queue === 'object' && queue.m ? queue.m : 'get'
+        debug(`fetch %s with %s`, url, JSON.stringify(params))
+        if (method === 'post') {
+          axios
+            .post(url, params)
+            .then(response => done(response.data))
+            .catch(console.error)
+        } else {
+          axios
+            .get(url, params)
+            .then(response => done(response.data))
+            .catch(console.error)
+        }
+      } else {
+        workers.splice(workers.indexOf(worker), 1)
+      }
+    }
+    workers.push(worker)
+    worker()
+  }
+  createWorker()
+  await new Promise(resolve => {
+    const interval = setInterval(() => {
+      if (workers.length === 0) {
+        clearInterval(interval)
+        resolve()
+      }
+    }, 500)
+  })
 }
