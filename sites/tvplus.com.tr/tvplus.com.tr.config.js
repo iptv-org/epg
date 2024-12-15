458cac0894a58d36d1055294b8ccada364418309
Toha <tohenk@yahoo.com>
2024-12-15 22:49:55 +0700

    Update tvplus.com.tr guide.
    Test:
    
    ```sh
    npm test -- tvplus.com.tr
    
    > test
    > run-script-os tvplus.com.tr
    
    > test:win32
    > SET "TZ=Pacific/Nauru" && npx jest --runInBand tvplus.com.tr
    
     PASS  sites/tvplus.com.tr/tvplus.com.tr.test.js
      √ can generate valid url (4 ms)
      √ can parse response (15 ms)
      √ can handle empty guide
    
    Test Suites: 1 passed, 1 total
    Tests:       3 passed, 3 total
    Snapshots:   0 total
    Time:        3.648 s, estimated 4 s
    Ran all test suites matching /tvplus.com.tr/i.
    ```
    
    Grab:
    
    ```sh
    npm run grab -- --site=tvplus.com.tr
    
    > grab
    > npx tsx scripts/commands/epg/grab.ts --site=tvplus.com.tr
    
    starting...
    config:
      output: guide.xml
      maxConnections: 1
      gzip: false
      site: tvplus.com.tr
    loading channels...
      found 10 channel(s)
    run #1:
      [1/20] tvplus.com.tr (tr) - 24TV.tr - Dec 15, 2024 (22 programs)
      [2/20] tvplus.com.tr (tr) - 24TV.tr - Dec 16, 2024 (19 programs)
      [3/20] tvplus.com.tr (tr) - BabyTV.uk - Dec 16, 2024 (48 programs)
      [4/20] tvplus.com.tr (tr) - BabyTV.uk - Dec 15, 2024 (48 programs)
      [5/20] tvplus.com.tr (tr) - ASpor.tr - Dec 16, 2024 (13 programs)
      [6/20] tvplus.com.tr (tr) - ASpor.tr - Dec 15, 2024 (14 programs)
      [7/20] tvplus.com.tr (tr) - APara.tr - Dec 16, 2024 (31 programs)
      [8/20] tvplus.com.tr (tr) - APara.tr - Dec 15, 2024 (25 programs)
      [9/20] tvplus.com.tr (tr) - AlJazeeraEnglish.qa - Dec 16, 2024 (32 programs)
      [10/20] tvplus.com.tr (tr) - AlJazeeraEnglish.qa - Dec 15, 2024 (32 programs)
      [11/20] tvplus.com.tr (tr) - AlJazeera.qa - Dec 16, 2024 (40 programs)
      [12/20] tvplus.com.tr (tr) - AlJazeera.qa - Dec 15, 2024 (41 programs)
      [13/20] tvplus.com.tr (tr) - AkitTV.tr - Dec 16, 2024 (15 programs)
      [14/20] tvplus.com.tr (tr) - AkitTV.tr - Dec 15, 2024 (12 programs)
      [15/20] tvplus.com.tr (tr) - AHaber.tr - Dec 16, 2024 (27 programs)
      [16/20] tvplus.com.tr (tr) - AHaber.tr - Dec 15, 2024 (19 programs)
      [17/20] tvplus.com.tr (tr) - A2TV.tr - Dec 16, 2024 (11 programs)
      [18/20] tvplus.com.tr (tr) - A2TV.tr - Dec 15, 2024 (11 programs)
      [19/20] tvplus.com.tr (tr) - 360.tr - Dec 16, 2024 (18 programs)
      [20/20] tvplus.com.tr (tr) - 360.tr - Dec 15, 2024 (18 programs)
      saving to "guide.xml"...
      done in 00h 02m 17s
    ```
    
    Signed-off-by: Toha <tohenk@yahoo.com>

--- sites/tvplus.com.tr/tvplus.com.tr.config.js
+++ sites/tvplus.com.tr/tvplus.com.tr.config.js
@@ -1,98 +1,100 @@
+const cheerio = require('cheerio')
 const axios = require('axios')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
+const debug = require('debug')('site:tvplus.com.tr')
 
 dayjs.extend(utc)
 dayjs.extend(customParseFormat)
 
+const channelsUrl = 'https://tvplus.com.tr/canli-tv/yayin-akisi'
+
 module.exports = {
   site: 'tvplus.com.tr',
   days: 2,
-  url: 'https://izmottvsc23.tvplus.com.tr:33207/EPG/JSON/PlayBillList',
   request: {
-    method: 'POST',
-    async headers() {
-      const response = await axios
-        .post('https://izmottvsc23.tvplus.com.tr:33207/EPG/JSON/Authenticate', {
-          terminaltype: 'WEBTV_WIDEVINE',
-          userType: '3',
-          timezone: 'UTC'
-        })
-        .catch(console.log)
-      const cookie = Array.isArray(response.headers['set-cookie'])
-        ? response.headers['set-cookie'].join('; ')
-        : ''
-
-      return { cookie }
-    },
-    data({ date, channel }) {
-      return {
-        type: '2',
-        channelid: channel.site_id,
-        begintime: date.format('YYYYMMDDHHmmss'),
-        endtime: date.add(1, 'd').format('YYYYMMDDHHmmss')
-      }
+    cache: {
+      ttl: 24 * 60 * 60 * 1000 // 1 day
     }
   },
-  parser: function ({ content }) {
-    let programs = []
-    const items = parseItems(content)
-    items.forEach(item => {
-      const start = parseStart(item)
-      const stop = parseStop(item)
-      programs.push({
-        title: item.name,
-        category: item.genres,
-        description: item.introduce,
-        image: parseImage(item),
-        start: start.toJSON(),
-        stop: stop.toJSON()
-      })
-    })
+  async url({ channel }) {
+    if (module.exports.buildId === undefined) {
+      module.exports.buildId = await module.exports.fetchBuildId()
+      debug('Got build id', module.exports.buildId)
+    }
+    const channelId = channel.site_id.replace('/', '--')
+    return `https://tvplus.com.tr/_next/data/${
+      module.exports.buildId
+    }/${
+      channel.lang
+    }/canli-tv/yayin-akisi/${
+      channelId
+    }.json?title=${
+      channelId
+    }`
+  },
+  parser({ content, date }) {
+    const programs = []
+    if (content) {
+      const data = JSON.parse(content)
+      if (Array.isArray(data?.pageProps?.allPlaybillList)) {
+        data.pageProps.allPlaybillList
+          .filter(i => i.length && i[0].starttime.startsWith(date.format('YYYY-MM-DD')))
+          .forEach(i => {
+            for (const schedule of i) {
+              const [, season, episode] = schedule.seasonInfo?.match(/(\d+)\. Sezon \- (\d+)\. Bölüm/) || [null, null, null]
+              programs.push({
+                title: schedule.name,
+                description: schedule.introduce,
+                category: schedule.genres,
+                image: schedule.picture,
+                season: season ? parseInt(season) : null,
+                episode: episode ? parseInt(episode) : null,
+                start: dayjs.utc(schedule.starttime),
+                stop: dayjs.utc(schedule.endtime)
+              })
+            }
+          })
+      }
+    }
 
     return programs
   },
   async channels() {
-    const cheerio = require('cheerio')
-
     const channels = []
     const data = await axios
-      .get(`https://tvplus.com.tr/canli-tv/yayin-akisi`)
+      .get(channelsUrl)
       .then(r => r.data)
-      .catch(console.log)
+      .catch(console.error)
 
     const $ = cheerio.load(data)
-    $('.channelListItem').each((i, el) => {
-      const name = $(el).find('.channelName').text()
-      const url = $(el).find('.channelLink').attr('href')
-      const [, site_id] = url.match(/\-\-(\d+)$/)
-
-      channels.push({
-        lang: 'tr',
-        name,
-        site_id
+    $('.channel-list-item a').toArray()
+      .forEach(el => {
+        const a = $(el)
+        channels.push({
+          lang: 'tr',
+          name: a.attr('title').replace(/Yayın Akışı/, '').trim(),
+          site_id: a.attr('href')
+            .replace(/\/canli\-tv\/yayin\-akisi\//, '')
+            .replace('--', '/') // change -- to / as it used in xml comment
+        })
       })
-    })
 
     return channels
+  },
+  async fetchBuildId() {
+    const data = await axios
+      .get(channelsUrl)
+      .then(r => r.data)
+      .catch(console.error)
+
+    if (data) {
+      const $ = cheerio.load(data)
+      const nextData = JSON.parse($('#__NEXT_DATA__').text())
+      return nextData?.buildId || null
+    } else {
+      return null
+    }
   }
-}
-
-function parseImage(item) {
-  return item.pictures && item.pictures.length ? item.pictures[0].href : null
-}
-
-function parseStart(item) {
-  return dayjs.utc(item.starttime, 'YYYYMMDDHHmmss')
-}
-
-function parseStop(item) {
-  return dayjs.utc(item.endtime, 'YYYYMMDDHHmmss')
-}
-
-function parseItems(content) {
-  const data = JSON.parse(content)
-
-  return data.playbilllist || []
 }
