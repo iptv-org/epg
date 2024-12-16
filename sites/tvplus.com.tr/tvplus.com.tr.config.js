ceaa777f5bbdab2b1a27e98f272a0aa37801e129
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-16 12:06:56 -0500

    Add files via upload

--- sites/tvplus.com.tr/tvplus.com.tr.config.js
+++ sites/tvplus.com.tr/tvplus.com.tr.config.js
@@ -1,4 +1,3 @@
-const axios = require('axios')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
@@ -9,90 +8,77 @@ dayjs.extend(customParseFormat)
 module.exports = {
   site: 'tvplus.com.tr',
   days: 2,
-  url: 'https://izmottvsc23.tvplus.com.tr:33207/EPG/JSON/PlayBillList',
   request: {
-    method: 'POST',
-    async headers() {
-      const response = await axios
-        .post('https://izmottvsc23.tvplus.com.tr:33207/EPG/JSON/Authenticate', {
-          terminaltype: 'WEBTV_WIDEVINE',
-          userType: '3',
-          timezone: 'UTC'
-        })
-        .catch(console.log)
-      const cookie = Array.isArray(response.headers['set-cookie'])
-        ? response.headers['set-cookie'].join('; ')
-        : ''
-
-      return { cookie }
-    },
-    data({ date, channel }) {
-      return {
-        type: '2',
-        channelid: channel.site_id,
-        begintime: date.format('YYYYMMDDHHmmss'),
-        endtime: date.add(1, 'd').format('YYYYMMDDHHmmss')
-      }
+    cache: {
+      ttl: 24 * 60 * 60 * 1000 // 1 day
     }
   },
-  parser: function ({ content }) {
-    let programs = []
-    const items = parseItems(content)
-    items.forEach(item => {
-      const start = parseStart(item)
-      const stop = parseStop(item)
-      programs.push({
-        title: item.name,
-        category: item.genres,
-        description: item.introduce,
-        image: parseImage(item),
-        start: start.toJSON(),
-        stop: stop.toJSON()
-      })
-    })
+  url({ channel }) {
+    const [buildId, slug, nr] = channel.site_id.split('/')
+    const channelId = [slug, nr].join('--')
+    return `https://tvplus.com.tr/_next/data/${
+      buildId
+    }/${
+      channel.lang
+    }/canli-tv/yayin-akisi/${
+      channelId
+    }.json?title=${
+      channelId
+    }`
+  },
+  parser({ content, date }) {
+    const programs = []
+    if (content) {
+      const data = JSON.parse(content)
+      if (Array.isArray(data?.pageProps?.allPlaybillList)) {
+        data.pageProps.allPlaybillList
+          .filter(i => i.length && i[0].starttime.startsWith(date.format('YYYY-MM-DD')))
+          .forEach(i => {
+            for (const schedule of i) {
+              const [, season, episode] = schedule.seasonInfo?.match(/(\d+)\. Sezon \- (\d+)\. Bölüm/) || [null, null, null]
+              programs.push({
+                title: schedule.name,
+                description: schedule.introduce,
+                category: schedule.genres,
+                image: schedule.picture,
+                season: season ? parseInt(season) : null,
+                episode: episode ? parseInt(episode) : null,
+                start: dayjs.utc(schedule.starttime),
+                stop: dayjs.utc(schedule.endtime)
+              })
+            }
+          })
+      }
+    }
 
     return programs
   },
   async channels() {
     const cheerio = require('cheerio')
-
+    const axios = require('axios')
     const channels = []
     const data = await axios
       .get(`https://tvplus.com.tr/canli-tv/yayin-akisi`)
       .then(r => r.data)
-      .catch(console.log)
+      .catch(console.error)
 
     const $ = cheerio.load(data)
-    $('.channelListItem').each((i, el) => {
-      const name = $(el).find('.channelName').text()
-      const url = $(el).find('.channelLink').attr('href')
-      const [, site_id] = url.match(/\-\-(\d+)$/)
-
-      channels.push({
-        lang: 'tr',
-        name,
-        site_id
+    const nextData = JSON.parse($('#__NEXT_DATA__').text())
+    $('.channel-list-item a').toArray()
+      .forEach(el => {
+        const a = $(el)
+        channels.push({
+          lang: 'tr',
+          name: a.attr('title').replace(/Yayın Akışı/, '').trim(),
+          site_id: [
+            nextData.buildId,
+            ...a.attr('href')
+              .replace(/\/canli\-tv\/yayin\-akisi\//, '')
+              .split('--')
+          ].join('/')
+        })
       })
-    })
 
     return channels
   }
-}
-
-function parseImage(item) {
-  return item.pictures && item.pictures.length ? item.pictures[0].href : null
-}
-
-function parseStart(item) {
-  return dayjs.utc(item.starttime, 'YYYYMMDDHHmmss')
-}
-
-function parseStop(item) {
-  return dayjs.utc(item.endtime, 'YYYYMMDDHHmmss')
-}
-
-function parseItems(content) {
-  const data = JSON.parse(content)
-
-  return data.playbilllist || []
 }
