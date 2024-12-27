3b542453abeb53d1db82993ccf42b8b08a6d4530
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-27 15:55:10 -0500

    Update cosmote.gr.test.js

--- sites/cosmote.gr/cosmote.gr.test.js
+++ sites/cosmote.gr/cosmote.gr.test.js
@@ -1,77 +1,81 @@
-const { parser, url } = require('./cosmote.gr.config.js')
-const fs = require('fs')
-const path = require('path')
+const { parser, url, channels } = require('./cosmote.gr.config.js')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
+const timezone = require('dayjs/plugin/timezone')
+const axios = require('axios')
 
 dayjs.extend(utc)
-dayjs.extend(timezone)
 dayjs.extend(customParseFormat)
+dayjs.extend(timezone)
 
-const date = dayjs.utc('2023-06-08', 'YYYY-MM-DD').startOf('d')
-const channel = {
-  site_id: '4e',
-  xmltv_id: '4E.gr'
+jest.mock('axios')
+
+const date = dayjs.utc('2024-12-26', 'YYYY-MM-DD').startOf('d')
+const channel = { site_id: 'vouli', xmltv_id: 'HellenicParliamentTV.gr' }
+
+const mockChannelData = {
+  "channels": [
+    {
+      "guid": "XTV100000954",
+      "title": "ΒΟΥΛΗ HD",
+      "callSign": "vouli",
+      "logos": {
+        "square": "https://tr.static.cdn.cosmotetvott.gr/ote-prod/channel_logos/vouli1-normal.png",
+        "wide": "https://tr.static.cdn.cosmotetvott.gr/ote-prod/channel_logos/vouli1-wide.png"
+      }
+    }
+  ]
+}
+
+const mockEpgData = {
+  "channels": [
+    {
+      "items": [
+        {
+          "startTime": "2024-12-26T23:00:00+00:00",
+          "endTime": "2024-12-27T00:00:00+00:00",
+          "title": "Τι Λέει ο Νόμος",
+          "description": "νημερωτική εκπομπή. Συζήτηση με τους εισηγητές των κομμάτων για το νομοθετικό έργο.",
+          "qoe": {
+            "genre": "Special"
+          },
+          "thumbnails": {
+            "standard": "https://gr-ermou-prod-cache05.static.cdn.cosmotetvott.gr/ote-prod/70/280/040029714812000800_1734415727199.jpg"
+          }
+        }
+      ]
+    }
+  ]
 }
 
 it('can generate valid url', () => {
-  expect(url({ channel, date })).toBe(
-    'https://www.cosmotetv.gr/portal/residential/program/epg/programchannel?p_p_id=channelprogram_WAR_OTETVportlet&p_p_lifecycle=0&_channelprogram_WAR_OTETVportlet_platform=IPTV&_channelprogram_WAR_OTETVportlet_date=08-06-2023&_channelprogram_WAR_OTETVportlet_articleTitleUrl=4e'
-  )
+  const startOfDay = dayjs(date).startOf('day').utc().unix()
+  const endOfDay = dayjs(date).endOf('day').utc().unix()
+  expect(url({ date, channel })).toBe(`https://mwapi-prod.cosmotetvott.gr/api/v3.4/epg/listings/el?from=${startOfDay}&to=${endOfDay}&callSigns=${channel.site_id}&endingIncludedInRange=false`)
 })
 
 it('can parse response', () => {
-  const content = fs.readFileSync(path.resolve(__dirname, '__data__/content1.html'))
-  const results = parser({ content, date }).map(p => {
-    p.start = p.start.toJSON()
-    p.stop = p.stop.toJSON()
+  const content = JSON.stringify(mockEpgData)
+  const result = parser({ date, content }).map(p => {
+    p.start = dayjs(p.start).toISOString()
+    p.stop = dayjs(p.stop).toISOString()
     return p
   })
 
-  expect(results[0]).toMatchObject({
-    start: '2023-06-07T20:30:00.000Z',
-    stop: '2023-06-07T21:45:00.000Z',
-    title: 'Τηλεφημερίδα',
-    category: 'Εκπομπή - Μαγκαζίνο'
-  })
-
-  expect(results[30]).toMatchObject({
-    start: '2023-06-08T19:45:00.000Z',
-    stop: '2023-06-08T20:30:00.000Z',
-    title: 'Μικρό Απόδειπνο',
-    category: 'Special'
-  })
-})
-
-it('can parse response when the guide starting before midnight', () => {
-  const content = fs.readFileSync(path.resolve(__dirname, '__data__/content2.html'))
-  const results = parser({ content, date }).map(p => {
-    p.start = p.start.toJSON()
-    p.stop = p.stop.toJSON()
-    return p
-  })
-
-  expect(results[0]).toMatchObject({
-    start: '2023-06-07T21:30:00.000Z',
-    stop: '2023-06-07T22:30:00.000Z',
-    title: 'Καλύτερα Αργά',
-    category: 'Ψυχαγωγική Εκπομπή'
-  })
-
-  expect(results[22]).toMatchObject({
-    start: '2023-06-08T19:00:00.000Z',
-    stop: '2023-06-08T21:30:00.000Z',
-    title: 'Πίσω Από Τις Γραμμές',
-    category: 'Εκπομπή - Μαγκαζίνο'
-  })
+  expect(result).toMatchObject([
+    {
+      title: "Τι Λέει ο Νόμος",
+      description: "Description 2",
+      category: "νημερωτική εκπομπή. Συζήτηση με τους εισηγητές των κομμάτων για το νομοθετικό έργο.",
+      image: "https://gr-ermou-prod-cache05.static.cdn.cosmotetvott.gr/ote-prod/70/280/040029714812000800_1734415727199.jpg",
+      start: "2024-12-26T23:00:00.000Z",
+      stop: "2024-12-27T00:00:00.000Z"
+    }
+  ])
 })
 
 it('can handle empty guide', () => {
-  const result = parser({
-    date,
-    content: '<!DOCTYPE html><html><head></head><body></body></html>'
-  })
+  const result = parser({ date, channel, content: '{"date":"2024-12-26","categories":[],"channels":[]}' });
   expect(result).toMatchObject([])
 })
