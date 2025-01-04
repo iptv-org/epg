d156add197ea6195e4a41e69ecfd0b92125c8189
freearhey <7253922+freearhey@users.noreply.github.com>
2025-01-04 17:01:53 +0300

    Update stod2.is.test.js

--- sites/stod2.is/stod2.is.test.js
+++ sites/stod2.is/stod2.is.test.js
@@ -16,42 +16,42 @@ const channel = { site_id: 'stod2', xmltv_id: 'Stod2.is' }
 
 const mockEpgData = JSON.stringify([
   {
-    "midill": "STOD2",
-    "midill_heiti": "Stöð 2",
-    "dagsetning": "2025-01-03T00:00:00Z",
-    "upphaf": "2025-01-03T08:00:00Z",
-    "titill": "Telma Borgþórsdóttir",
-    "isltitill": "Heimsókn",
-    "undirtitill": "Telma Borgþórsdóttir",
-    "seria": 8,
-    "thattur": 5,
-    "thattafjoldi": 10,
-    "birta_thatt": 1,
-    "opin": 0,
-    "beint": 0,
-    "frumsyning": 0,
-    "framundan_i_beinni": 0,
-    "tegund": "SER",
-    "flokkur": "Icelandic",
-    "adalhlutverk": "",
-    "leikstjori": "",
-    "ar": "2019",
-    "bannad": "Green",
-    "recidefni": 592645105,
-    "recidlidur": 592645184,
-    "recidsyning": null,
-    "refno": null,
-    "frelsi": 0,
-    "netdagar": 0,
-    "lysing": "Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.",
-    "slott": 15,
-    "slotlengd": "00:15"
+    midill: 'STOD2',
+    midill_heiti: 'Stöð 2',
+    dagsetning: '2025-01-03T00:00:00Z',
+    upphaf: '2025-01-03T08:00:00Z',
+    titill: 'Telma Borgþórsdóttir',
+    isltitill: 'Heimsókn',
+    undirtitill: 'Telma Borgþórsdóttir',
+    seria: 8,
+    thattur: 5,
+    thattafjoldi: 10,
+    birta_thatt: 1,
+    opin: 0,
+    beint: 0,
+    frumsyning: 0,
+    framundan_i_beinni: 0,
+    tegund: 'SER',
+    flokkur: 'Icelandic',
+    adalhlutverk: '',
+    leikstjori: '',
+    ar: '2019',
+    bannad: 'Green',
+    recidefni: 592645105,
+    recidlidur: 592645184,
+    recidsyning: null,
+    refno: null,
+    frelsi: 0,
+    netdagar: 0,
+    lysing:
+      'Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.',
+    slott: 15,
+    slotlengd: '00:15'
   }
 ])
 
 it('can generate valid url', () => {
   const generatedUrl = url({ date, channel })
-  console.log('Generated URL:', generatedUrl)
   expect(generatedUrl).toBe('https://api.stod2.is/dagskra/api/stod2/2025-01-03')
 })
 
@@ -65,13 +65,14 @@ it('can parse response', () => {
 
   expect(result).toMatchObject([
     {
-      title: "Heimsókn",
-      sub_title: "Telma Borgþórsdóttir",
-      description: "Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.",
-      actors: "",
-      directors: "",
-      start: "2025-01-03T08:00:00.000Z",
-      stop: "2025-01-03T08:15:00.000Z"
+      title: 'Heimsókn',
+      sub_title: 'Telma Borgþórsdóttir',
+      description:
+        'Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.',
+      actors: '',
+      directors: '',
+      start: '2025-01-03T08:00:00.000Z',
+      stop: '2025-01-03T08:15:00.000Z'
     }
   ])
 })
