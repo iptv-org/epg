bbd299999cb33f79bdaff333a1975e9effe11df1
Michael Feinbier <michaelfeinbier@users.noreply.github.com>
2025-01-05 01:01:53 +0100

    feat: Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:lts-bookworm-slim AS build
+
+WORKDIR /build
+
+COPY yarn.lock ./yarn.lock
+COPY package.json ./package.json
+COPY package-lock.json ./package-lock.json
+
+RUN npm ci --ignore-scripts
+
+COPY . .
+COPY docker/ecosystem.config.js ./ecosystem.config.js
+COPY docker/serve.json ./serve.json
+
+RUN npm install pm2 -g \
+    npm run postinstall
+
+# Set some application defaults
+ENV NODE_ENV=production \
+    OUTPUT=/build/public/guide.xml \
+    GZIP=true
+
+EXPOSE 3000
+CMD [ "pm2-runtime", "ecosystem.config.js" ]
---
+++ docker/ecosystem.config.js
@@ -0,0 +1,32 @@
+// Docker startup configuration
+
+// first add the server app
+let apps = [{
+        name: "serve",
+        script: "npm run serve",
+    }
+]
+
+// if we run in cron-mode then add 2 jobs
+if (process.env.CRON) {
+    // run an initial grab at startup
+    apps.push({
+        name: "grab-startup",
+        script: "npm run grab",
+        autorestart: false,
+        filter_env: ["CRON"],
+    })
+    // run a grab according to the cron schedule
+    apps.push({
+        name: "grab-cron",
+        script: "npm run grab",
+    })
+} else {
+    // one time run only
+    apps.push({
+        name: "grab",
+        script: "npm run grab",
+    })
+}
+
+module.exports = { apps }
