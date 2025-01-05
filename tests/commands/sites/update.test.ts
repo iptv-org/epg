25093cb92716ea23ae1c69cde87693b4c559e335
freearhey <7253922+freearhey@users.noreply.github.com>
2025-01-05 19:20:33 +0300

    Update update.test.ts

--- tests/commands/sites/update.test.ts
+++ tests/commands/sites/update.test.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process'
 import fs from 'fs-extra'
 import path from 'path'
+import os from 'os'
 
 beforeEach(() => {
   fs.emptyDirSync('tests/__data__/output')
@@ -16,12 +17,14 @@ beforeEach(() => {
 })
 
 it('can update SITES.md', () => {
-  const stdout = execSync(
-    'DOT_SITES_DIR=tests/__data__/output/.sites SITES_DIR=tests/__data__/input/sites-update/sites npm run sites:update',
-    {
-      encoding: 'utf8'
-    }
-  )
+  let ENV_VAR =
+    'DOT_SITES_DIR=tests/__data__/output/.sites SITES_DIR=tests/__data__/input/sites-update/sites'
+  if (os.platform() === 'win32') {
+    ENV_VAR =
+      'SET "DOT_SITES_DIR=tests/__data__/output/.sites" && SET "SITES_DIR=tests/__data__/input/sites-update/sites" &&'
+  }
+
+  const stdout = execSync(`${ENV_VAR} npm run sites:update`, { encoding: 'utf8' })
   if (process.env.DEBUG === 'true') console.log(stdout)
 
   expect(content('tests/__data__/output/sites.md')).toEqual(
