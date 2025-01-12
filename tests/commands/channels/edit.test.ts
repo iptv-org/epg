bc4b5bd187378e9d6614a4d858fa3162178f6677
freearhey <7253922+freearhey@users.noreply.github.com>
2025-01-12 23:09:49 +0300

    Update edit.test.ts

--- tests/commands/channels/edit.test.ts
+++ tests/commands/channels/edit.test.ts
@@ -3,11 +3,6 @@ import { execSync } from 'child_process'
 import os from 'os'
 import { pathToFileURL } from 'node:url'
 
-type ExecError = {
-  status: number
-  stdout: string
-}
-
 let ENV_VAR = 'DATA_DIR=tests/__data__/input/temp/data'
 if (os.platform() === 'win32') {
   ENV_VAR = 'SET "DATA_DIR=tests/__data__/input/temp/data" &&'
@@ -23,26 +18,16 @@ beforeEach(() => {
 
 describe('channels:edit', () => {
   it('shows list of options for a channel', () => {
-    try {
-      const cmd = `${ENV_VAR} npm run channels:edit --- tests/__data__/output/channels.xml`
-      const stdout = execSync(cmd, { encoding: 'utf8' })
-      if (process.env.DEBUG === 'true') console.log(cmd, stdout)
-    } catch (error) {
-      if (process.env.DEBUG === 'true') console.log(cmd, error)
-      expect((error as ExecError).status).toBe(1)
-      expect((error as ExecError).stdout).toContain('CNN International | CNNInternational.us [new]')
-      expect((error as ExecError).stdout).toContain(
-        'CNN International Europe | CNNInternationalEurope.us'
-      )
-      expect((error as ExecError).stdout).toContain('Overwrite')
-      expect((error as ExecError).stdout).toContain('Skip')
-      expect((error as ExecError).stdout).toContain(
-        "File 'tests/__data__/output/channels.xml' successfully saved"
-      )
-      expect(content('tests/__data__/output/channels.xml')).toEqual(
-        content('tests/__data__/expected/sites/channels-edit/example.com.channels.xml')
-      )
-    }
+    const cmd = `${ENV_VAR} npm run channels:edit --- tests/__data__/output/channels.xml`
+    const stdout = execSync(cmd, { encoding: 'utf8' })
+    if (process.env.DEBUG === 'true') console.log(cmd, stdout)
+    expect(stdout).toContain('CNN International Europe | CNNInternationalEurope.us')
+    expect(stdout).toContain('Type...')
+    expect(stdout).toContain('Skip')
+    expect(stdout).toContain("File 'tests/__data__/output/channels.xml' successfully saved")
+    expect(content('tests/__data__/output/channels.xml')).toEqual(
+      content('tests/__data__/expected/sites/channels-edit/example.com.channels.xml')
+    )
   })
 })
 
