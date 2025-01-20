de1dbc68d0fe08bb3089393b22594063e5c92e5e
freearhey <7253922+freearhey@users.noreply.github.com>
2025-01-21 00:22:12 +0300

    Update movistarplus.es.config.js

--- sites/movistarplus.es/movistarplus.es.config.js
+++ sites/movistarplus.es/movistarplus.es.config.js
@@ -1,65 +1,60 @@
-const { DateTime } = require('luxon')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const dayjs = require('dayjs')
 
 module.exports = {
   site: 'movistarplus.es',
   days: 2,
-  url: function ({ date }) {
-    return `https://www.movistarplus.es/programacion-tv/${date.format('YYYY-MM-DD')}?v=json`
+  url({ channel, date }) {
+    return `https://www.movistarplus.es/programacion-tv/${channel.site_id}/${date.format(
+      'YYYY-MM-DD'
+    )}`
   },
-  parser({ content, channel, date }) {
+  parser({ content }) {
     let programs = []
-    let items = parseItems(content, channel)
+    let items = parseItems(content)
     if (!items.length) return programs
-    let guideDate = date
-    items.forEach(item => {
-      let startTime = DateTime.fromFormat(
-        `${guideDate.format('YYYY-MM-DD')} ${item.HORA_INICIO}`,
-        'yyyy-MM-dd HH:mm',
-        {
-          zone: 'Europe/Madrid'
-        }
-      ).toUTC()
-      let stopTime = DateTime.fromFormat(
-        `${guideDate.format('YYYY-MM-DD')} ${item.HORA_FIN}`,
-        'yyyy-MM-dd HH:mm',
-        {
-          zone: 'Europe/Madrid'
-        }
-      ).toUTC()
-      if (stopTime < startTime) {
-        guideDate = guideDate.add(1, 'd')
-        stopTime = stopTime.plus({ days: 1 })
-      }
+    items.forEach(el => {
       programs.push({
-        title: item.TITULO,
-        category: item.GENERO,
-        start: startTime,
-        stop: stopTime
+        title: el.item.name,
+        start: dayjs(el.item.startDate),
+        stop: dayjs(el.item.endDate)
       })
     })
     return programs
   },
   async channels() {
-    const axios = require('axios')
-    const dayjs = require('dayjs')
-    const data = await axios
-      .get(`https://www.movistarplus.es/programacion-tv/${dayjs().format('YYYY-MM-DD')}?v=json`)
+    const html = await axios
+      .get('https://www.movistarplus.es/programacion-tv')
       .then(r => r.data)
       .catch(console.log)
 
-    return Object.values(data.data).map(item => {
+    const $ = cheerio.load(html)
+    let scheme = $('script:contains(ItemList)').html()
+    scheme = JSON.parse(scheme)
+
+    return scheme.itemListElement.map(el => {
+      const urlParts = el.item.url.split('/')
+      const site_id = urlParts.pop().toLowerCase()
+
       return {
         lang: 'es',
-        site_id: item.DATOS_CADENA.CODIGO,
-        name: item.DATOS_CADENA.NOMBRE
+        name: el.item.name,
+        site_id
       }
     })
   }
 }
 
-function parseItems(content, channel) {
-  const json = typeof content === 'string' ? JSON.parse(content) : content
-  if (!(`${channel.site_id}-CODE` in json.data)) return []
-  const data = json.data[`${channel.site_id}-CODE`]
-  return data ? data.PROGRAMAS : []
+function parseItems(content) {
+  try {
+    const $ = cheerio.load(content)
+    let scheme = $('script:contains("@type": "ItemList")').html()
+    scheme = JSON.parse(scheme)
+    if (!scheme || !Array.isArray(scheme.itemListElement)) return []
+
+    return scheme.itemListElement
+  } catch {
+    return []
+  }
 }
