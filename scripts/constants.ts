81f2665235f64973b66cebe87b084abe7ea8a3a1
freearhey <7253922+freearhey@users.noreply.github.com>
2025-01-21 18:51:04 +0300

    Update constants.ts

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,3 +1,4 @@
+export const ROOT_DIR = process.env.ROOT_DIR || '.'
 export const SITES_DIR = process.env.SITES_DIR || './sites'
 export const GUIDES_DIR = process.env.GUIDES_DIR || './guides'
 export const DATA_DIR = process.env.DATA_DIR || './temp/data'
