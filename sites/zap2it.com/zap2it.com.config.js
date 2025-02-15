e75dab9553cdd24e096f7c4531c425cef52d0098
Newbiect <51307314+Newbiect@users.noreply.github.com>
2025-02-15 17:50:38 -0500

    Update zap2it.com.config.js

--- sites/zap2it.com/zap2it.com.config.js
+++ sites/zap2it.com/zap2it.com.config.js
@@ -65,6 +65,7 @@ module.exports = {
       })
     })
 
-    return programs.filter(p => dayjs(p.start).add(dayjs(p.start).utcOffset(), 'minute').isBetween(date.startOf('day').add(5, 'hour').utc(), date.endOf('day').add(5, 'hour').utc(), 'second', '[]'))
+    return programs.filter(p => dayjs(p.start).add(dayjs(p.start).utcOffset(), 'minute').isBetween(date.startOf('day').subtract(dayjs().utcOffset(), 'minute').utc(), 
+    date.endOf('day').subtract(dayjs().utcOffset(), 'minute').utc(), 'second', '[]'))
   }
 }
