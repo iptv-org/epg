4bdcdf0962f0f2183cef7d8779deb4538123dd31
freearhey <7253922+freearhey@users.noreply.github.com>
2025-03-31 09:10:44 +0300

    Update tvtv.us.test.js

--- sites/tvtv.us/tvtv.us.test.js
+++ sites/tvtv.us/tvtv.us.test.js
@@ -50,115 +50,115 @@ it('can parse response', async () => {
     start: '2025-01-30T00:30:00.000Z',
     stop: '2025-01-30T01:00:00.000Z',
     title: 'The Big Bang Theory',
-    subtitle: 'The Bow Tie Asymmetry',
-    description:
-      "When Amy's parents and Sheldon's family arrive, everybody is focused on making sure the wedding arrangements go according to plan -- everyone except the bride and groom.",
-    image: 'https://tvtv.us/gn/pi/assets/p185554_b_v11_az.jpg?w=240&h=360',
-    date: '2018',
-    season: 11,
-    episode: 24,
-    actors: [
-      {
-        value: 'Johnny Galecki',
-        role: 'Leonard Hofstadter'
-      },
-      {
-        value: 'Jim Parsons',
-        role: 'Sheldon Cooper'
-      },
-      {
-        value: 'Kaley Cuoco',
-        role: 'Penny'
-      },
-      {
-        value: 'Simon Helberg',
-        role: 'Howard Wolowitz'
-      },
-      {
-        value: 'Kunal Nayyar',
-        role: 'Raj Koothrappali'
-      },
-      {
-        value: 'Mayim Bialik',
-        role: 'Amy Farrah Fowler'
-      },
-      {
-        value: 'Melissa Rauch',
-        role: 'Bernadette Rostenkowski'
-      },
-      {
-        value: 'Kevin Sussman',
-        role: 'Stuart',
-        guest: 'yes'
-      },
-      {
-        value: 'Laurie Metcalf',
-        role: 'Mary',
-        guest: 'yes'
-      },
-      {
-        value: 'John Ross Bowie',
-        role: 'Kripke',
-        guest: 'yes'
-      },
-      {
-        value: 'Wil Wheaton',
-        role: 'Himself',
-        guest: 'yes'
-      },
-      {
-        value: 'Brian Posehn',
-        role: 'Bert',
-        guest: 'yes'
-      },
-      {
-        value: "Jerry O'Connell",
-        role: 'George',
-        guest: 'yes'
-      },
-      {
-        value: 'Courtney Henggeler',
-        role: 'Missy',
-        guest: 'yes'
-      },
-      {
-        value: 'Lauren Lapkus',
-        role: 'Denise',
-        guest: 'yes'
-      },
-      {
-        value: 'Teller',
-        role: 'Mr. Fowler',
-        guest: 'yes'
-      },
-      {
-        value: 'Kathy Bates',
-        role: 'Mrs. Fowler',
-        guest: 'yes'
-      },
-      {
-        value: 'Mark Hamill',
-        role: 'Himself',
-        guest: 'yes'
-      }
-    ],
-    directors: ['Mark Cendrowski'],
-    producers: ['Chuck Lorre', 'Bill Prady', 'Steven Molaro'],
-    writers: [
-      'Chuck Lorre',
-      'Steven Molaro',
-      'Maria Ferrari',
-      'Steve Holland',
-      'Eric Kaplan',
-      'Tara Hernandez'
-    ],
-    categories: ['Sitcom'],
-    ratings: [
-      {
-        value: 'TVPG',
-        system: 'USA Parental Rating'
-      }
-    ]
+    subtitle: 'The Bow Tie Asymmetry'
+    // description:
+    //   "When Amy's parents and Sheldon's family arrive, everybody is focused on making sure the wedding arrangements go according to plan -- everyone except the bride and groom.",
+    // image: 'https://tvtv.us/gn/pi/assets/p185554_b_v11_az.jpg?w=240&h=360',
+    // date: '2018',
+    // season: 11,
+    // episode: 24,
+    // actors: [
+    //   {
+    //     value: 'Johnny Galecki',
+    //     role: 'Leonard Hofstadter'
+    //   },
+    //   {
+    //     value: 'Jim Parsons',
+    //     role: 'Sheldon Cooper'
+    //   },
+    //   {
+    //     value: 'Kaley Cuoco',
+    //     role: 'Penny'
+    //   },
+    //   {
+    //     value: 'Simon Helberg',
+    //     role: 'Howard Wolowitz'
+    //   },
+    //   {
+    //     value: 'Kunal Nayyar',
+    //     role: 'Raj Koothrappali'
+    //   },
+    //   {
+    //     value: 'Mayim Bialik',
+    //     role: 'Amy Farrah Fowler'
+    //   },
+    //   {
+    //     value: 'Melissa Rauch',
+    //     role: 'Bernadette Rostenkowski'
+    //   },
+    //   {
+    //     value: 'Kevin Sussman',
+    //     role: 'Stuart',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Laurie Metcalf',
+    //     role: 'Mary',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'John Ross Bowie',
+    //     role: 'Kripke',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Wil Wheaton',
+    //     role: 'Himself',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Brian Posehn',
+    //     role: 'Bert',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: "Jerry O'Connell",
+    //     role: 'George',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Courtney Henggeler',
+    //     role: 'Missy',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Lauren Lapkus',
+    //     role: 'Denise',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Teller',
+    //     role: 'Mr. Fowler',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Kathy Bates',
+    //     role: 'Mrs. Fowler',
+    //     guest: 'yes'
+    //   },
+    //   {
+    //     value: 'Mark Hamill',
+    //     role: 'Himself',
+    //     guest: 'yes'
+    //   }
+    // ],
+    // directors: ['Mark Cendrowski'],
+    // producers: ['Chuck Lorre', 'Bill Prady', 'Steven Molaro'],
+    // writers: [
+    //   'Chuck Lorre',
+    //   'Steven Molaro',
+    //   'Maria Ferrari',
+    //   'Steve Holland',
+    //   'Eric Kaplan',
+    //   'Tara Hernandez'
+    // ],
+    // categories: ['Sitcom'],
+    // ratings: [
+    //   {
+    //     value: 'TVPG',
+    //     system: 'USA Parental Rating'
+    //   }
+    // ]
   })
 })
 
