588bb8b4ff136b29b7eb1e60cac42b90f8d6a7f4
freearhey <7253922+freearhey@users.noreply.github.com>
2025-11-01 12:01:37 +0300

    Update default.config.js

--- scripts/default.config.js
+++ scripts/default.config.js
@@ -1,6 +1,11 @@
 export default {
+  output: 'guide.xml',
   days: 1,
   delay: 0,
+  maxConnections: 1,
+  curl: false,
+  gzip: false,
+  debug: false,
   request: {
     maxContentLength: 5242880,
     timeout: 30000,
