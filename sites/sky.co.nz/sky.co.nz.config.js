0f506fb52f1d5f89f0a394173fad677d673faa33
theofficialomega <30985701+BellezaEmporium@users.noreply.github.com>
2025-12-15 00:45:10 +0100

    fix sky.nz and add new channels for sky.com

--- sites/sky.co.nz/sky.co.nz.config.js
+++ sites/sky.co.nz/sky.co.nz.config.js
@@ -1,22 +1,76 @@
 const axios = require('axios')
 const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const tz = require('dayjs/plugin/timezone')
+const timezone = 'Pacific/Auckland'
+
+dayjs.extend(utc)
+dayjs.extend(tz)
+
 
 module.exports = {
   site: 'sky.co.nz',
   days: 2,
-  url({ date, channel }) {
-    return `https://web-epg.sky.co.nz/prod/epgs/v1?channelNumber=${
-      channel.site_id
-    }&start=${date.valueOf()}&end=${date.add(1, 'day').valueOf()}&limit=20000`
+  url: 'https://api.skyone.co.nz/exp/graph',
+  request: {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data({ date } = {}) {
+      return ({
+        query: `{
+            experience(appId: TV_GUIDE_WEB) {
+              channelGroup(id: "4b7LA20J4iHaThwky9iVqn") {
+                id
+                title
+                channels {
+                  ... on LinearChannel {
+                    id
+                    title
+                    number
+                    tileImage {
+                      uri
+                    }
+                    slotsForDay(date: "${dayjs.tz(date || dayjs(), timezone).format('YYYY-MM-DD')}") {
+                      slots {
+                        id
+                        startMs
+                        endMs
+                        live
+                        nzBsaRatingString
+                        programme {
+                          ... on Episode {
+                            id
+                            title
+                            synopsis
+                          }
+                          ... on Movie {
+                            id
+                            title
+                          }
+                          ... on PayPerViewEventProgram {
+                            id
+                            title
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }`
+      })
+    }
   },
-  parser({ content }) {
+  parser({ content, channel }) {
     let programs = []
-    const items = parseItems(content)
+    const items = parseItems(content, channel)
     items.forEach(item => {
       programs.push({
         title: item.title,
         description: item.synopsis,
-        category: item.genres,
         rating: parseRating(item),
         start: dayjs(parseInt(item.start)),
         stop: dayjs(parseInt(item.end))
@@ -31,19 +85,41 @@ module.exports = {
       .then(r => r.data)
       .catch(console.log)
 
-    return data.channels.map(item => {
+    return data.map(item => {
       return {
         lang: 'en',
         site_id: parseInt(item.number).toString(),
-        name: item.name
+        name: item.sort
       }
     })
   }
 }
 
-function parseItems(content) {
-  const data = JSON.parse(content)
-  return data && data.events && Array.isArray(data.events) ? data.events : []
+function parseItems(content, channel) {
+  if (!channel?.site_id) return []
+
+  let data
+  try {
+    data = JSON.parse(content)
+  } catch {
+    return []
+  }
+
+  const channels = data?.data?.experience?.channelGroup?.channels
+  
+  if (!Array.isArray(channels)) return []
+  
+  const channelData = channels.find(i => i.number === parseInt(channel.site_id))
+
+  if (!channelData?.slotsForDay?.slots) return []
+  
+  return channelData.slotsForDay.slots.map(slot => ({
+    title: slot.programme.title || null,
+    synopsis: slot.programme.synopsis || null,
+    rating: slot.nzBsaRatingString || null,
+    start: slot.startMs,
+    end: slot.endMs
+  }))
 }
 
 function parseRating(item) {
--- sites/sky.co.nz/sky.co.nz.test.js
+++ sites/sky.co.nz/sky.co.nz.test.js
@@ -1,60 +1,74 @@
-const { parser, url } = require('./sky.co.nz.config.js')
+const { parser, url, request } = require('./sky.co.nz.config.js')
 const fs = require('fs')
 const path = require('path')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
+const tz = require('dayjs/plugin/timezone')
 dayjs.extend(utc)
+dayjs.extend(tz)
 
-const date = dayjs.utc('2023-01-21', 'YYYY-MM-DD').startOf('d')
 const channel = {
-  site_id: '36',
-  xmltv_id: 'SkyMoviesFamily.nz'
+  site_id: '1',
+  xmltv_id: 'TVNZ 1'
 }
 it('can generate valid url', () => {
-  expect(url({ date, channel })).toBe(
-    'https://web-epg.sky.co.nz/prod/epgs/v1?channelNumber=36&start=1674259200000&end=1674345600000&limit=20000'
-  )
+  expect(url).toBe('https://api.skyone.co.nz/exp/graph')
+})
+
+it('can generate valid request method', () => {
+  expect(request.method).toBe('POST')
 })
 
 it('can parse response', () => {
   const content = fs.readFileSync(path.resolve(__dirname, '__data__/content.json'))
-  const result = parser({ content }).map(p => {
+  const result = parser({ content, channel }).map(p => {
     p.start = p.start.toJSON()
     p.stop = p.stop.toJSON()
     return p
   })
 
   expect(result[0]).toMatchObject({
-    title: 'Sing 2',
+    title: 'Hard Quiz',
     description:
-      "Animated: Buster Moon and his friends must persuade the world's most reclusive rock star to help launch their most dazzling extravaganza yet. Voices Of: Matthew McConaughey, Reese Witherspoon (2021)",
-    category: ['Animated'],
+      "What do King Henry VII, Shaquille O'Neal, dinosaurs and The Adventures of Priscilla, Queen of the Desert have in common? They're all expert topics on tonight's #HardQuiz!",
     rating: { system: 'OFLC', value: 'PG' },
-    start: '2023-01-20T23:41:00.000Z',
-    stop: '2023-01-21T01:28:00.000Z'
+    start: '2025-12-12T10:40:00.000Z',
+    stop: '2025-12-12T11:15:00.000Z'
   })
 
-  expect(result[5]).toMatchObject({
-    title: 'Harry Potter and the Goblet of Fire',
+  expect(result[33]).toMatchObject({
+    title: 'Obituary',
     description:
-      'Adventure: Harry is selected to represent Hogwarts at a legendary and dangerous wizardry competition between three schools of magic. Stars: Daniel Radcliffe, Rupert Grint (2005)',
-    category: ['Action/Adventure'],
-    rating: { system: 'OFLC', value: 'M-V' },
-    start: '2023-01-21T07:42:00.000Z',
-    stop: '2023-01-21T10:13:00.000Z'
+      'Season Finale: When Ward reveals what really happened the night Maria Riedle was killed, Elvira sets about framing Hughie for the crime. S1 E6',
+    rating: { system: 'OFLC', value: '16' },
+    start: '2025-12-13T10:55:00.000Z',
+    stop: '2025-12-13T11:50:00.000Z'
   })
 })
 
+// Not easy to mock this case, as it's the same repeated message (for all channels ?) 
+// with the time space in the message. Only what doesn't change is provided, but will still pass the test out.
 it('can handle empty guide', () => {
-  const result = parser(
-    {
-      content: `{
-        "code": "DATE_FORMAT_ERROR",
-        "description": "DateFormat error",
-        "message": "Unparseable date: x"
-        }`
-    },
+  const result = parser({
+    content: `{
+        "locations": [
+          {
+            "line": 14,
+            "column": 11
+          }
+        ],
+        "path": [
+          "experience",
+          "channelGroup",
+          "channels",
+          0,
+          "slotsForDay"
+        ],
+        "extensions": {
+          "classification": "DataFetchingException"
+        }
+      }`,
     channel
-  )
+  })
   expect(result).toMatchObject([])
 })
