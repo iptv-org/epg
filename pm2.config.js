4b80b78ac265498e7d4c6e5b21b42a66c7205e6c
freearhey <7253922+freearhey@users.noreply.github.com>
2026-02-20 15:36:06 +0300

    Fixed path to channels.xml

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ const grab = process.env.SITE
   ? `npm run grab -- --site=${process.env.SITE} ${
       process.env.CLANG ? `--lang=${process.env.CLANG}` : ''
     } --output=public/guide.xml`
-  : 'npm run grab -- --channels=channels.xml --output=public/guide.xml'
+  : 'npm run grab -- --channels=public/channels.xml --output=public/guide.xml'
 
 const apps = [
   {
