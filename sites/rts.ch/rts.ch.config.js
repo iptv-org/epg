0987bdf5c1bd0ebaf1fc97216af67b675c6eb0e3
David ROBIN <darobin@gmail.com>
2026-03-26 14:51:02 +0100

    Revert "Update rts.ch source with SRF and RSI channels"
    This reverts commit 91abdc7f9e0a4e3ceed1ff00f09b4c8304c91589.

--- sites/rts.ch/rts.ch.config.js
+++ sites/rts.ch/rts.ch.config.js
@@ -1,27 +1,12 @@
 const axios = require('axios')
 const dayjs = require('dayjs')
 
-const LANG_BY_VENDOR = {
-  RTS: 'fr',
-  SRF: 'de',
-  RSI: 'it',
-  RTR: 'rm',
-}
-
-const CHANNELS_APIS = [
-  'https://www.rts.ch/play/v3/api/rts/production/tv-program-guide',
-  'https://www.srf.ch/play/v3/api/srf/production/tv-program-guide',
-  'https://www.rsi.ch/play/v3/api/rsi/production/tv-program-guide',
-]
-
 module.exports = {
   site: 'rts.ch',
   days: 2,
 
   url({ channel, date }) {
-    const vendor = channel.site_id.split('|')[0].toLowerCase()
-    const channelId = channel.site_id.split('|')[1]
-    return `https://il.srgssr.ch/integrationlayer/2.0/${vendor}/programGuide/tv/byDate/${date.format('YYYY-MM-DD')}?reduced=false&channelId=${channelId}`
+    return `https://il.srgssr.ch/integrationlayer/2.0/rts/programGuide/tv/byDate/${date.format('YYYY-MM-DD')}?reduced=false&channelId=${channel.site_id}`
   },
 
   parser({ content }) {
@@ -45,24 +30,13 @@ module.exports = {
 
   async channels() {
     const today = dayjs().format('YYYY-MM-DD')
-    const results = []
-
-    for (const apiUrl of CHANNELS_APIS) {
-      try {
-        const { data } = await axios.get(`${apiUrl}?date=${today}`)
-        for (const entry of data.data) {
-          const vendor = entry.channel.vendor  // e.g. "RTS", "SRF"
-          results.push({
-            site_id: `${vendor}|${entry.channel.id}`,
-            name:    entry.channel.title,
-            lang:    LANG_BY_VENDOR[vendor] || 'fr',
-          })
-        }
-      } catch {
-        // skip unavailable sources
-      }
-    }
-
-    return results
+    const { data } = await axios.get(
+      `https://www.rts.ch/play/v3/api/rts/production/tv-program-guide?date=${today}`
+    )
+    return data.data.map(entry => ({
+      site_id: entry.channel.id,
+      name:    entry.channel.title,
+      lang:    'fr',
+    }))
   }
 }
