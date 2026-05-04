3f6b8267cce6af117d72cc19adb900c7b94d7c72
freearhey <7253922+freearhey@users.noreply.github.com>
2026-05-04 18:01:35 +0300

    Update tests

--- tests/commands/workers/load.test.ts
+++ tests/commands/workers/load.test.ts
@@ -3,21 +3,22 @@ import { pathToFileURL } from 'node:url'
 import fs from 'fs-extra'
 
 const ENV_VAR =
-  'cross-env CURR_DATE=2022-10-04 ROOT_DIR=tests/__data__/output DATA_DIR=tests/__data__/input/guides_update'
+  'cross-env CURR_DATE=2022-10-04 ROOT_DIR=tests/__data__/output DATA_DIR=tests/__data__/output/'
 
 beforeEach(() => {
   fs.emptyDirSync('tests/__data__/output')
+  fs.copySync('tests/__data__/input/workers_load/workers.txt', 'tests/__data__/output/workers.txt')
 })
 
-describe('guides:update', () => {
-  it('can update GUIDES.md', () => {
-    const cmd = `${ENV_VAR} npm run guides:update`
+describe('workers:load', () => {
+  it('can create workers.json', () => {
+    const cmd = `${ENV_VAR} npm run workers:load`
 
     const stdout = execSync(cmd, { encoding: 'utf8' })
     if (process.env.DEBUG === 'true') console.log(cmd, stdout)
 
-    expect(content('tests/__data__/output/GUIDES.md')).toEqual(
-      content('tests/__data__/expected/guides_update/GUIDES.md')
+    expect(content('tests/__data__/output/workers.json')).toEqual(
+      content('tests/__data__/expected/workers_load/workers.json')
     )
   })
 })
