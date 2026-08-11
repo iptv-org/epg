c6b0f5acaff5176f5404877fc358e91674ff7bff
Michaël Arnauts <michael.arnauts@gmail.com>
2026-08-11 10:14:45 +0200

    update telenet

--- sites/telenet.tv/telenet.tv.test.js
+++ sites/telenet.tv/telenet.tv.test.js
@@ -78,12 +78,44 @@ it('can parse response', async () => {
       'Sharon Gless'
     ],
     season: 5,
-    episode: 8
+    episode: 8,
+    date: '2005',
+    country: 'US',
+    rating: { system: 'Kijkwijzer', value: '16' },
+    language: ['nl'],
+    subtitles: [],
+    new: false
+  })
+})
+
+it('can parse response without program details', async () => {
+  const content = fs.readFileSync(path.resolve(__dirname, '__data__/content_0000.json'))
+
+  axios.get.mockImplementation(() => Promise.resolve({ data: '' }))
+
+  let results = await parser({ content, channel, date })
+  results = results.map(p => {
+    p.start = p.start.toJSON()
+    p.stop = p.stop.toJSON()
+    return p
+  })
+
+  expect(results[0]).toMatchObject({
+    start: '2022-10-29T23:56:00.000Z',
+    stop: '2022-10-30T01:44:00.000Z',
+    title: 'Queer as Folk USA',
+    rating: { system: 'Kijkwijzer', value: '16' },
+    language: ['nl']
   })
 })
 
 it('can handle empty guide', async () => {
-  let results = await parser({ content: '', channel, date })
+  // a date of its own, the segments of `date` are still held by the cache of the parser
+  const emptyDate = dayjs.utc('2022-11-30', 'YYYY-MM-DD').startOf('d')
+
+  axios.get.mockImplementation(() => Promise.resolve({ data: '' }))
+
+  let results = await parser({ content: '', channel, date: emptyDate })
 
   expect(results).toMatchObject([])
 })
